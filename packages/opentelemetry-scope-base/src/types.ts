9d41576ff6b7c437ddd83821ab4b54d05279ab9d
Valentin Marchaud <contact@vmarchaud.fr>
2019-07-11 18:40:17 +0200

    Add BaseScopeManager interface (#66)
    * Add context-base and context-asynchooks packages
    
    * add BaseScopeManager interface #46

---
+++ packages/opentelemetry-scope-base/src/types.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export interface BaseScopeManager {
+  /**
+   * Get the current active scope
+   */
+  active(): unknown;
+
+  /**
+   * Run the fn callback with object set as the current active scope
+   * @param scope Any object to set as the current active scope
+   * @param fn A callback to be imediately run within a specific scope
+   */
+  with<T extends (...args: unknown[]) => unknown>(
+    scope: unknown,
+    fn: T
+  ): ReturnType<T>;
+
+  /**
+   * Bind an object as the current scope (or a specific one)
+   * @param object Object to which a scope need to be set
+   * @param [scope] Optionaly specify the scope which you want to assign
+   */
+  bind<T>(object: T, scope?: unknown): T;
+
+  /**
+   * Enable scope management
+   */
+  enable(): this;
+
+  /**
+   * Disable scope management
+   */
+  disable(): void;
+}
