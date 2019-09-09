e097a97c290a82ae64a3d6fe4ea5c6f19b557512
Mark Wolff <mark.wolff@microsoft.com>
2019-09-09 19:32:02 -0400

    feat(baseplugin): add internalfilesloader

--- packages/opentelemetry-core/src/trace/instrumentation/BasePlugin.ts
+++ packages/opentelemetry-core/src/trace/instrumentation/BasePlugin.ts
@@ -15,13 +15,34 @@
  */
 
 import { Tracer, Plugin, Logger } from '@opentelemetry/types';
+import * as semver from 'semver';
+import * as path from 'path';
+
+/**
+ * Maps a name (key) representing a internal file module and its exports
+ */
+export interface ModuleNameToFilePath {
+  [moduleName: string]: string; // moduleName: path/to/file
+}
+
+/**
+ * Maps a semver to a module:filepath Map
+ */
+export interface ModuleExportsMapping {
+  [semver: string]: ModuleNameToFilePath;
+}
 
 /** This class represent the base to patch plugin. */
 export abstract class BasePlugin<T> implements Plugin<T> {
+  supportedVersions?: string[];
   protected _moduleExports!: T;
   protected _tracer!: Tracer;
   protected _logger!: Logger;
-  supportedVersions?: string[];
+  protected readonly _internalFilesList?: ModuleExportsMapping
+  protected _internalFilesExports!: { [module: string]: any };
+  protected basedir!: string;
+
+  constructor(readonly moduleName?: string, readonly version?: string) {}
 
   enable(
     moduleExports: T,
@@ -32,11 +53,56 @@ export abstract class BasePlugin<T> implements Plugin<T> {
     this._moduleExports = moduleExports;
     this._tracer = tracer;
     this._logger = logger;
+    if (config && config.basedir) this.basedir = config.basedir as string;
+    this._internalFilesExports = this._loadInternalFiles();
     return this.patch();
   }
 
   disable(): void {
     this.unpatch();
+  }
+
+  private _loadInternalFiles(): ModuleExportsMapping {
+    let extraModules: ModuleExportsMapping = {};
+    if (this._internalFilesList) {
+      this._logger.debug('loadInternalFiles %o', this._internalFilesList);
+      Object.keys(this._internalFilesList).forEach(versionRange => {
+        if (this.version && this.moduleName && semver.satisfies(this.version, versionRange)) {
+          if (Object.keys(extraModules).length > 0) {
+            this._logger.warn(
+              'Plugin for %s@%s, has overlap version range (%s) for internal files: %o',
+              this.moduleName,
+              this.version,
+              versionRange,
+              this._internalFilesList
+            );
+          }
+          extraModules = this._loadInternalModuleFiles(this._internalFilesList![versionRange], this.basedir);
+        }
+      });
+    }
+    if (Object.keys(extraModules)) {
+      this._logger.debug('No internal files could be loaded for %s@%s', this.moduleName, this.version);
+    }
+    return extraModules;
+  }
+
+  private _loadInternalModuleFiles(extraModulesList: ModuleNameToFilePath, basedir: string): ModuleExportsMapping {
+    const extraModules: ModuleExportsMapping = {};
+    if (extraModulesList) {
+      Object.keys(extraModulesList).forEach(moduleName => {
+        try {
+          this._logger.debug('loading File %s', extraModulesList[moduleName]);
+          extraModules[moduleName] = require(path.join(
+            basedir,
+            extraModulesList[moduleName]
+          ));
+        } catch (e) {
+          this._logger.error('Could not load internal file %s of module %s. Error: %s', path.join(basedir, extraModulesList[moduleName]), this.moduleName, e.message);
+        }
+      });
+    }
+    return extraModules;
   }
 
   protected abstract patch(): T;
---
+++ packages/opentelemetry-core/test/trace/fixtures/test-package/foo/bar/internal.js
@@ -0,0 +1,5 @@
+Object.defineProperty(exports, "__esModule", { value: true });
+function internallyExportedFunction() {
+    return true;
+}
+exports.internallyExportedFunction = internallyExportedFunction;
