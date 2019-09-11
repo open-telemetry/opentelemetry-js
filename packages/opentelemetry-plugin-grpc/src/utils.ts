c675c8683a77a50affb01949019b2a8725644a5f
Mark Wolff <mark.wolff@microsoft.com>
2019-09-11 15:13:44 -0400

    feat(plugin): add grpc plugin (#215)
    * feat: add grpc plugin
    
    * fix: remove test typo
    
    * fix: nyc missing from test
    
    * fix: linting and NodeTracer export
    
    * chore: rename grpc test file
    
    * test: add error code tests
    
    * refactor: logger is given to plugin
    
    * fix(grpc): using logger before defined
    
    * refactor: move private statics to internal file
    
    * test: add withSpan OK test
    
    * refactor: follow plugin naming pattern
    
    * refactor(tests): replace ProxyTracer
    
    * refactor(grpc): move helper funcs to utils
    
    * refactor(tests): use new memory exporter helper

---
+++ packages/opentelemetry-plugin-grpc/src/utils.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { CanonicalCode, Status } from '@opentelemetry/types';
+import * as grpcModule from 'grpc'; // For types only
+
+// Equivalent to lodash _.findIndex
+export const findIndex: <T>(args: T[], fn: (arg: T) => boolean) => number = (
+  args,
+  fn: Function
+) => {
+  let index = -1;
+  for (const arg of args) {
+    index++;
+    if (fn(arg)) {
+      return index;
+    }
+  }
+  return -1;
+};
+
+/**
+ * Convert a grpc status code to an opentelemetry Canonical code. For now, the enums are exactly the same
+ * @param status
+ */
+export const _grpcStatusCodeToCanonicalCode = (
+  status?: grpcModule.status
+): CanonicalCode => {
+  if (status !== 0 && !status) {
+    return CanonicalCode.UNKNOWN;
+  }
+  return status as number;
+};
+
+export const _grpcStatusCodeToSpanStatus = (status: number): Status => {
+  return { code: status };
+};
---
+++ packages/opentelemetry-plugin-grpc/test/utils/assertionUtils.ts
@@ -0,0 +1,67 @@
+/**
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { SpanKind } from '@opentelemetry/types';
+import * as assert from 'assert';
+import { AttributeNames } from '../../src/enums/AttributeNames';
+import { GrpcPlugin } from '../../src/grpc';
+import * as grpc from 'grpc';
+import { ReadableSpan } from '@opentelemetry/basic-tracer';
+
+export const assertSpan = (
+  span: ReadableSpan,
+  kind: SpanKind,
+  validations: { name: string; status: grpc.status }
+) => {
+  assert.strictEqual(span.spanContext.traceId.length, 32);
+  assert.strictEqual(span.spanContext.spanId.length, 16);
+  assert.strictEqual(span.kind, kind);
+
+  assert.strictEqual(
+    span.attributes[AttributeNames.COMPONENT],
+    GrpcPlugin.component
+  );
+  assert.ok(span.endTime);
+  assert.strictEqual(span.links.length, 0);
+  assert.strictEqual(span.events.length, 1);
+
+  assert.ok(span.startTime < span.endTime);
+  assert.ok(span.endTime > 0);
+
+  if (span.kind === SpanKind.SERVER) {
+    assert.ok(span.spanContext);
+  }
+
+  // validations
+  assert.strictEqual(span.name, validations.name);
+  assert.strictEqual(span.status.code, validations.status);
+};
+
+// Check if sourceSpan was propagated to targetSpan
+export const assertPropagation = (
+  incomingSpan: ReadableSpan,
+  outgoingSpan: ReadableSpan
+) => {
+  const targetSpanContext = incomingSpan.spanContext;
+  const sourceSpanContext = outgoingSpan.spanContext;
+  assert.strictEqual(targetSpanContext.traceId, sourceSpanContext.traceId);
+  assert.strictEqual(incomingSpan.parentSpanId, sourceSpanContext.spanId);
+  assert.strictEqual(
+    targetSpanContext.traceOptions,
+    sourceSpanContext.traceOptions
+  );
+  assert.notStrictEqual(targetSpanContext.spanId, sourceSpanContext.spanId);
+};
