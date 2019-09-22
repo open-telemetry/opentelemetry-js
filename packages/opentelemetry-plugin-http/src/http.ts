66f33d74dfe6b66a00faa8714eb7c289a83f2b04
Olivier Albertini <olivier.albertini@montreal.ca>
2019-09-22 13:35:32 -0400

    fix: change options to config based on BasePlugin
    closes #313
    
    Signed-off-by: Olivier Albertini <olivier.albertini@montreal.ca>

--- packages/opentelemetry-plugin-http/src/http.ts
+++ packages/opentelemetry-plugin-http/src/http.ts
@@ -43,13 +43,11 @@ import { Utils } from './utils';
  */
 export class HttpPlugin extends BasePlugin<Http> {
   static readonly component = 'http';
-  options!: HttpPluginConfig;
+  protected _config!: HttpPluginConfig;
 
   constructor(readonly moduleName: string, readonly version: string) {
     super();
-    // TODO: remove this once options will be passed
-    // see https://github.com/open-telemetry/opentelemetry-js/issues/210
-    this.options = {};
+    this._config = {};
   }
 
   /** Patches HTTP incoming and outcoming request functions. */
@@ -204,8 +202,8 @@ export class HttpPlugin extends BasePlugin<Http> {
 
             span.setAttributes(attributes);
 
-            if (this.options.applyCustomAttributesOnSpan) {
-              this.options.applyCustomAttributesOnSpan(span, request, response);
+            if (this._config.applyCustomAttributesOnSpan) {
+              this._config.applyCustomAttributesOnSpan(span, request, response);
             }
 
             span.end();
@@ -244,7 +242,7 @@ export class HttpPlugin extends BasePlugin<Http> {
 
       plugin._logger.debug('%s plugin incomingRequest', plugin.moduleName);
 
-      if (Utils.isIgnored(pathname, plugin.options.ignoreIncomingPaths)) {
+      if (Utils.isIgnored(pathname, plugin._config.ignoreIncomingPaths)) {
         return original.apply(this, [event, ...args]);
       }
 
@@ -309,8 +307,8 @@ export class HttpPlugin extends BasePlugin<Http> {
             .setAttributes(attributes)
             .setStatus(Utils.parseResponseStatus(response.statusCode));
 
-          if (plugin.options.applyCustomAttributesOnSpan) {
-            plugin.options.applyCustomAttributesOnSpan(span, request, response);
+          if (plugin._config.applyCustomAttributesOnSpan) {
+            plugin._config.applyCustomAttributesOnSpan(span, request, response);
           }
 
           span.end();
@@ -346,7 +344,7 @@ export class HttpPlugin extends BasePlugin<Http> {
       options = optionsParsed;
 
       if (
-        Utils.isIgnored(origin + pathname, plugin.options.ignoreOutgoingUrls)
+        Utils.isIgnored(origin + pathname, plugin._config.ignoreOutgoingUrls)
       ) {
         return original.apply(this, [options, ...args]);
       }
