bb87739d58916f51fa8973a2aaa492e5cbebe3cf
Mark Wolff <mark.wolff@microsoft.com>
2019-09-25 14:49:42 -0700

    fix: typo

--- examples/grpc/client.js
+++ examples/grpc/client.js
@@ -5,7 +5,7 @@ const config = require('./setup');
 
 /**
  * The trace instance needs to be initialized first, if you want to enable
- * automatic tracing for built-in plugins (HTTP in this case).
+ * automatic tracing for built-in plugins (gRPC in this case).
  */
 config.setupTracerAndExporters('grpc-client-service');
 
