a54e5909f37b7d292b98fbc3d75225e6dab5271e
Mayur Kale <mayurkale@google.com>
2019-10-02 11:28:05 -0700

    fix: remove nock

--- benchmark/benchmark.js
+++ benchmark/benchmark.js
@@ -2,12 +2,10 @@
 
 const Benchmark = require('benchmark');
 const benchmarks = require('beautify-benchmark');
-const nock = require('nock');
 
 Benchmark.options.maxTime = 0;
+// @todo : Change it to between 50-100 or keep it random.
 Benchmark.options.minSamples = 5;
-
-nock.disableNetConnect();
 
 module.exports = () => {
   const suite = new Benchmark.Suite();
