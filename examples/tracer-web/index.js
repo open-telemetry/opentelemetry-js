21785a37dba70b88d996b641a82f30fbc9c6d18e
Bartlomiej Obecny <bobecny@gmail.com>
2019-10-28 21:29:58 +0100

    chore: updated example for web tracer

--- examples/tracer-web/index.js
+++ examples/tracer-web/index.js
@@ -39,53 +39,69 @@ webTracerWithZone.withSpan(span1, () => {
   console.log('Current span is span1', webTracerWithZone.getCurrentSpan() === span1);
 });
 
-const span3 = webTracerWithZone.startSpan('foo1');
-webTracerWithZone.withSpan(span3, () => {
-  console.log('Current span is span3', webTracerWithZone.getCurrentSpan() === span3);
-  setTimeout(() => {
-    const span4 = webTracerWithZone.startSpan('foo2');
-    console.log('Current span is span3', webTracerWithZone.getCurrentSpan() === span3);
-    webTracerWithZone.withSpan(span4, () => {
-      console.log('Current span is span4', webTracerWithZone.getCurrentSpan() === span4);
-      setTimeout(() => {
-        console.log('Current span is span4', webTracerWithZone.getCurrentSpan() === span4);
-      }, 500);
-    });
-    // there is a timeout which still keeps span4 active
-    console.log('Current span is span4', webTracerWithZone.getCurrentSpan() === span4);
-  }, 490);
-  console.log('Current span is span3', webTracerWithZone.getCurrentSpan() === span3);
-});
-
-console.log('Current span is window', webTracerWithZone.getCurrentSpan() === window);
-
 // example of keeping track of scope between async operations
+let counter = 0;
 const prepareClickEvent = () => {
-  const element = document.getElementById('button2');
+  const url = 'https://raw.githubusercontent.com/open-telemetry/opentelemetry-js/master/package.json';
+
+  const element = document.getElementById('button1');
   let mainSpan = webTracerWithZone.startSpan('main-span');
   webTracerWithZone.bind(element, mainSpan);
 
   const onClick = () => {
-    element.removeEventListener('click', onClick);
-    console.log('starting timeout, scope is', webTracerWithZone.getCurrentSpan().name);
-    const spanClickAsync = webTracerWithZone.startSpan('span-click-timeout', {
+    counter++;
+    const span1 = webTracerWithZone.startSpan(`files-series-info-${counter}`, {
       parent: webTracerWithZone.getCurrentSpan()
     });
-    spanClickAsync.addEvent('starting timeout');
 
-    window.setTimeout(() => {
-      console.log('timeout finished, scope is', webTracerWithZone.getCurrentSpan().name);
-      spanClickAsync.addEvent('timeout finished');
-      console.log('ending spans');
-      spanClickAsync.end();
-      mainSpan.end();
-    }, 500);
+    webTracerWithZone.withSpan(span1, ()=> {
+      span1.addEvent('fetching-data-1');
+      getData(url).then((data)=> {
+        console.log(webTracerWithZone.getCurrentSpan().name, data.description, data.version);
+        webTracerWithZone.getCurrentSpan().addEvent('fetching-data-1-completed');
+        webTracerWithZone.getCurrentSpan().addEvent('fetching-data-2');
+
+        getData(url).then((data)=> {
+          console.log(webTracerWithZone.getCurrentSpan().name, data.description, data.version);
+
+          webTracerWithZone.getCurrentSpan().addEvent('fetching-data-2-completed');
+          webTracerWithZone.getCurrentSpan().end();
+          counter++
+          ;const span2 = webTracerWithZone.startSpan(`files-series-info-${counter}`, {
+            parent: webTracerWithZone.getCurrentSpan()
+          });
+          webTracerWithZone.withSpan(span2, ()=> {
+            webTracerWithZone.getCurrentSpan().addEvent('fetching-data-3');
+            getData(url).then((data)=> {
+              console.log(webTracerWithZone.getCurrentSpan().name, data.description, data.version);
+              webTracerWithZone.getCurrentSpan().addEvent('fetching-data-3-completed');
+              webTracerWithZone.getCurrentSpan().end();
+            });
+          });
+        });
+      });
+    });
   };
   element.addEventListener('click', onClick);
+};
 
+const getData = (url) => {
+  return new Promise(async (resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.send();
+    req.onload = function () {
+      let json;
+      try {
+        json = JSON.parse(req.responseText);
+      } catch (e) {
+        reject(e);
+      }
+      resolve(json);
+    };
+  });
 };
 
 window.addEventListener('load', () => {
-  const element = document.getElementById('button1');
-  element.addEventListener('click', prepareClickEvent);
+  prepareClickEvent();
 });
