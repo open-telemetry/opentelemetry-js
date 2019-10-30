b26f810de48f8a87cb29a21df5fe343d56ec0744
Daniel Dyla <dyladan@users.noreply.github.com>
2019-10-30 09:28:00 -0400

    feat(prometheus-exporter): counter and gauge

--- packages/opentelemetry-exporter-prometheus/src/prometheus.ts
+++ packages/opentelemetry-exporter-prometheus/src/prometheus.ts
@@ -21,19 +21,15 @@ import { createServer, Server, IncomingMessage, ServerResponse } from 'http';
 
 import * as types from '@opentelemetry/types';
 
-import { ReadableMetric, MetricDescriptor } from '@opentelemetry/metrics';
+import { ReadableMetric, MetricDescriptor, MetricExporter, MetricDescriptorType, LabelValue } from '@opentelemetry/metrics';
 
 import { ExporterConfig } from './types';
 import { NoopLogger } from '@opentelemetry/core';
+import { ExportResult } from '@opentelemetry/base';
 
 import * as Prometheus from 'prom-client';
 
-import {
-  Registry,
-} from 'prom-client';
-
-export class PrometheusExporter {
-
+export class PrometheusExporter implements MetricExporter {
   static readonly DEFAULT_OPTIONS = {
     port: 9464,
     startServer: false,
@@ -41,12 +37,12 @@ export class PrometheusExporter {
     prefix: '',
   };
 
-  private readonly _registry = new Registry();
+  private readonly _registry = new Prometheus.Registry();
   private readonly _logger: types.Logger;
   private readonly _port: number;
   private readonly _endpoint: string;
   private _server?: Server;
-  private readonly _prefix: string;
+  private readonly _prefix?: string;
 
   // Histogram cannot have a label named 'le'
   // private static readonly RESERVED_HISTOGRAM_LABEL = 'le';
@@ -59,7 +55,7 @@ export class PrometheusExporter {
   constructor(config: ExporterConfig = {}, callback?: () => void) {
     this._logger = config.logger || new NoopLogger();
     this._port = config.port || PrometheusExporter.DEFAULT_OPTIONS.port;
-    // this._prefix = config.prefix || PrometheusExporter.DEFAULT_OPTIONS.prefix;
+    this._prefix = config.prefix || PrometheusExporter.DEFAULT_OPTIONS.prefix;
 
     let endpoint = config.endpoint || PrometheusExporter.DEFAULT_OPTIONS.endpoint;
 
@@ -73,29 +69,104 @@ export class PrometheusExporter {
     }
   }
 
-  export(metrics: ReadableMetric[]) {
+  export(readableMetrics: ReadableMetric[], cb: (result: ExportResult) => void) {
+    this._logger.debug('Prometheus exporter export');
 
-    metrics.forEach((metric) => {
-      this._updateMetric(metric);
-    });
+    for (const readableMetric of readableMetrics) {
+      this._updateMetric(readableMetric);
+    }
+
+    cb(ExportResult.SUCCESS);
   }
 
-  private _updateMetric(metric: ReadableMetric) {
-    this._registerMetric(metric);
+  private _updateMetric(readableMetric: ReadableMetric) {
+    const metric = this._registerMetric(readableMetric);
+    if (!metric) return;
+
+    const labelKeys = readableMetric.descriptor.labelKeys;
+
+    if (metric instanceof Prometheus.Counter) {
+      for (const ts of readableMetric.timeseries) {
+        metric.inc(
+          this._getLabelValues(labelKeys, ts.labelValues),
+          ts.points[0].value as number
+        );
+      }
+    }
+
+    if (metric instanceof Prometheus.Gauge) {
+      for (const ts of readableMetric.timeseries) {
+        metric.set(
+          this._getLabelValues(labelKeys, ts.labelValues),
+          ts.points[0].value as number
+        );
+      }
+    }
+
+    // TODO: only counter and gauge are implemented in metrics so far
   }
 
-  private _registerMetric(metric: ReadableMetric): Prometheus.Metric {
-    const metricName = this._getPrometheusMetricName(metric.descriptor);
+  private _getLabelValues(keys: string[], values: LabelValue[]) {
+    const labelValues: Prometheus.labelValues = {};
+    for (let i = 0; i < keys.length; i++) {
+      if (values[i].value !== null) {
+        labelValues[keys[i]] = values[i].value!;
+      }
+    }
+    return labelValues;
+  }
+
+  private _registerMetric(readableMetric: ReadableMetric): Prometheus.Metric | undefined {
+    const metricName = this._getPrometheusMetricName(readableMetric.descriptor);
+    const metric = this._registry.getSingleMetric(metricName);
+    if (metric) return metric;
+
+    const newMetric = this._newMetric(readableMetric, metricName);
+    if (!newMetric) return;
+
+    this._registry.registerMetric(newMetric);
+    return newMetric;
+  }
+
+  private _newMetric(readableMetric: ReadableMetric, name: string): Prometheus.Metric | undefined {
+    const metricObject = {
+      name,
+      help: readableMetric.descriptor.description,
+      labelNames: readableMetric.descriptor.labelKeys
+    };
+
+    switch (readableMetric.descriptor.type) {
+      case MetricDescriptorType.COUNTER_DOUBLE:
+      case MetricDescriptorType.COUNTER_INT64:
+        return new Prometheus.Counter(metricObject);
+      case MetricDescriptorType.GAUGE_DOUBLE:
+      case MetricDescriptorType.GAUGE_INT64:
+        return new Prometheus.Gauge(metricObject);
+      case MetricDescriptorType.GAUGE_HISTOGRAM:
+      case MetricDescriptorType.CUMULATIVE_HISTOGRAM:
+        const histogramConfig = {
+          ...metricObject,
+          buckets: this._getHistogramBoundaries(readableMetric)
+        };
+        return new Prometheus.Histogram(histogramConfig);
+      case MetricDescriptorType.SUMMARY:
+        return new Prometheus.Summary(metricObject);
+      case MetricDescriptorType.UNSPECIFIED:
+        this._logger.error("Do not use UNSPECIFIED readable metric type");
+        return;
+    }
+
+  }
+
+  private _getHistogramBoundaries(_readableMetric: ReadableMetric): number[] {
+    // TODO
+    throw new Error('Unimplemented')
   }
 
   private _getPrometheusMetricName(descriptor: MetricDescriptor): string {
-    let metricName;
-    if (this._prefix) {
-      metricName = `${this._prefix}_${descriptor.name}`;
-    } else {
-      metricName = descriptor.name;
-    }
-    return this._sanitizePrometheusMetricName(metricName);
+    return this._sanitizePrometheusMetricName(
+      this._prefix ? `${this._prefix}_${descriptor.name}` : descriptor.name
+    );
   }
 
   private _sanitizePrometheusMetricName(name: string): string {
