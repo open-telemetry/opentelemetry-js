2e203510c48bd8bb00785d58f24e604a0780acc4
Bartlomiej Obecny <bobecny@gmail.com>
2019-11-01 21:56:28 +0100

    chore: updating karma files

--- packages/opentelemetry-scope-zone-peer-dep/test/index-webpack.ts
+++ packages/opentelemetry-scope-zone-peer-dep/test/index-webpack.ts
@@ -16,6 +16,8 @@
 
 // This file is the webpack entry point for the browser Karma tests. It requires
 // all modules ending in "test" from the current folder and all its subfolders.
-
 const testsContext = require.context('.', true, /test$/);
 testsContext.keys().forEach(testsContext);
+
+const srcContext = require.context('.', true, /src$/);
+srcContext.keys().forEach(srcContext);
