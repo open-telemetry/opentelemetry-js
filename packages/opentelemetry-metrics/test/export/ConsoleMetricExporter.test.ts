be6ae7d202bb0e52b8eec97e3634f6e90b3f4849
Mayur Kale <mayurkale@google.com>
2019-11-06 10:12:46 -0800

    feat: add ConsoleMetricExporter

---
+++ packages/opentelemetry-metrics/test/export/ConsoleMetricExporter.test.ts
@@ -0,0 +1,56 @@
+/*!
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import { ConsoleMetricExporter, Meter, GaugeMetric } from '../../src';
+
+describe('ConsoleMetricExporter', () => {
+  let consoleExporter: ConsoleMetricExporter;
+  let previousConsoleLog: any;
+
+  beforeEach(() => {
+    previousConsoleLog = console.log;
+    console.log = () => {};
+    consoleExporter = new ConsoleMetricExporter();
+  });
+
+  afterEach(() => {
+    console.log = previousConsoleLog;
+  });
+
+  describe('.export()', () => {
+    it('should export information about metrics', () => {
+      const spyConsole = sinon.spy(console, 'log');
+
+      const meter = new Meter();
+      meter.addExporter(consoleExporter);
+      const gauge = meter.createGauge('gauge', {
+        description: 'a test description',
+        labelKeys: ['key1'],
+      }) as GaugeMetric;
+      const handle = gauge.getHandle(['labelValue1']);
+      handle.set(10);
+      const [descriptor, timeseries] = spyConsole.args;
+      assert.deepStrictEqual(descriptor, [
+        { description: 'a test description', name: 'gauge' },
+      ]);
+      assert.deepStrictEqual(timeseries, [
+        { labels: [{ value: 'labelValue1' }], value: 10 },
+      ]);
+    });
+  });
+});
