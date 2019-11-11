3d01be67fd7279ec451123ec421b015a172a9db3
Bartlomiej Obecny <bobecny@gmail.com>
2019-11-11 17:53:13 +0100

    feat(scope-zone): new scope manager to support async operations in web (#461)
    * feat(scope-zone): new scope manager to support async operations in web
    
    * chore: removing not needed dependency
    
    * chore: updating readme
    
    * chore: clean up
    
    * chore: refactored the bind method
    
    * chore: updated example for web tracer
    
    * chore: updated readme
    
    * chore: updating jsdoc
    
    * fix: fixing flaky test for BatchSpanProcessor with timeout
    
    * chore: missing link
    
    * chore: updating karma files
    
    * chore: adding docs and tests for utils
    
    * chore: refactoring zone scope manager
    
    * chore: adding test for web and zone scope manager for parallel actions
    
    * chore: updating example for web tracer with zone scope manager
    
    * chore: updating example for web tracer with zone scope manager
    
    * chore: adding test for parallel run

--- examples/tracer-web/index.js
+++ examples/tracer-web/index.js
@@ -1,11 +1,83 @@
 import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { WebTracer } from '@opentelemetry/web';
 import { DocumentLoad } from '@opentelemetry/plugin-document-load';
+import { ZoneScopeManager } from '@opentelemetry/scope-zone';
 
 const webTracer = new WebTracer({
   plugins: [
     new DocumentLoad()
   ]
 });
-
 webTracer.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+const webTracerWithZone = new WebTracer({
+  scopeManager: new ZoneScopeManager(),
+  plugins: [
+    new DocumentLoad()
+  ]
+});
+webTracerWithZone.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+
+console.log('Current span is window', webTracerWithZone.getCurrentSpan() === window);
+
+// example of keeping track of scope between async operations
+const prepareClickEvent = () => {
+  const url1 = 'https://raw.githubusercontent.com/open-telemetry/opentelemetry-js/master/package.json';
+  const url2 = 'https://raw.githubusercontent.com/open-telemetry/opentelemetry-js/master/packages/opentelemetry-web/package.json';
+
+  const element = document.getElementById('button1');
+  let mainSpan = webTracerWithZone.startSpan('main-span');
+  webTracerWithZone.bind(element, mainSpan);
+
+  const onClick = () => {
+    const span1 = webTracerWithZone.startSpan(`files-series-info-1`, {
+      parent: webTracerWithZone.getCurrentSpan()
+    });
+
+    const span2 = webTracerWithZone.startSpan(`files-series-info-2`, {
+      parent: webTracerWithZone.getCurrentSpan()
+    });
+
+    webTracerWithZone.withSpan(span1, () => {
+      getData(url1).then((data) => {
+        console.log('current span is span1', webTracerWithZone.getCurrentSpan() === span1);
+        console.log('info from package.json', data.description, data.version);
+        webTracerWithZone.getCurrentSpan().addEvent('fetching-span1-completed');
+        span1.end();
+      });
+    });
+
+    webTracerWithZone.withSpan(span2, () => {
+      getData(url2).then((data) => {
+        setTimeout(() => {
+          console.log('current span is span2', webTracerWithZone.getCurrentSpan() === span2);
+          console.log('info from package.json', data.description, data.version);
+          webTracerWithZone.getCurrentSpan().addEvent('fetching-span2-completed');
+          span2.end();
+        }, 100);
+      });
+    });
+  };
+  element.addEventListener('click', onClick);
+};
+
+const getData = (url) => {
+  return new Promise(async (resolve, reject) => {
+    const req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.send();
+    req.onload = function () {
+      let json;
+      try {
+        json = JSON.parse(req.responseText);
+      } catch (e) {
+        reject(e);
+      }
+      resolve(json);
+    };
+  });
+};
+
+window.addEventListener('load', () => {
+  prepareClickEvent();
+});
--- packages/opentelemetry-web/test/WebTracer.test.ts
+++ packages/opentelemetry-web/test/WebTracer.test.ts
@@ -21,6 +21,7 @@ import { BasicTracerConfig } from '@opentelemetry/tracing';
 import { WebTracerConfig } from '../src';
 import { StackScopeManager } from '../src/StackScopeManager';
 import { WebTracer } from '../src/WebTracer';
+import { ZoneScopeManager } from '@opentelemetry/scope-zone';
 
 class DummyPlugin extends BasePlugin<unknown> {
   patch() {}
@@ -76,6 +77,47 @@ describe('WebTracer', () => {
     it('should work without default scope manager', () => {
       assert.doesNotThrow(() => {
         tracer = new WebTracer({});
+      });
+    });
+
+    describe('when scopeManager is "ZoneScopeManager"', () => {
+      it('should correctly return the scopes for 2 parallel actions', () => {
+        const webTracerWithZone = new WebTracer({
+          scopeManager: new ZoneScopeManager(),
+        });
+
+        const rootSpan = webTracerWithZone.startSpan('rootSpan');
+
+        webTracerWithZone.withSpan(rootSpan, () => {
+          assert.ok(
+            webTracerWithZone.getCurrentSpan() === rootSpan,
+            'Current span is rootSpan'
+          );
+          const concurrentSpan1 = webTracerWithZone.startSpan(
+            'concurrentSpan1'
+          );
+          const concurrentSpan2 = webTracerWithZone.startSpan(
+            'concurrentSpan2'
+          );
+
+          webTracerWithZone.withSpan(concurrentSpan1, () => {
+            setTimeout(() => {
+              assert.ok(
+                webTracerWithZone.getCurrentSpan() === concurrentSpan1,
+                'Current span is concurrentSpan1'
+              );
+            }, 10);
+          });
+
+          webTracerWithZone.withSpan(concurrentSpan2, () => {
+            setTimeout(() => {
+              assert.ok(
+                webTracerWithZone.getCurrentSpan() === concurrentSpan2,
+                'Current span is concurrentSpan2'
+              );
+            }, 20);
+          });
+        });
       });
     });
   });
