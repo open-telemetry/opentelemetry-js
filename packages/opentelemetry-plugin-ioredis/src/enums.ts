3ca2df4ac3b1e2b19fa99a75d8c1a2c8a122ab26
Naseem <naseemkullah@gmail.com>
2019-12-24 08:58:50 -0500

    Add ioredis (#558)
    * Add ioredis
    
    * remove unused util
    
    * Console error when callback cannot be found in command object
    
    * fix
    
    * There never seems to be a this.address
    
    * err can be made non-optional here since it is currently always passed in
    
    * No address property
    
    * Update packages
    
    * Adjust value of DB_STATEMENT
    
    * Add DB_TYPE attribute
    
    * Handle async/await ioredis commands that have no cb
    
    * Fix promise handling and adjust attributes
    
    * Use template literal
    
    * Add same comments as pg plugin for promise handling
    
    * Use endSpan  function
    
    * fix: combine 2 lines into 1
    
    * chore: update modules
    
    * fix: tests
    
    * style: rename error
    
    * docs: update min version
    
    * fix: nullish coalescing
    
    * Revert "fix: nullish coalescing"
    
    This reverts commit 16a1f402444e43198c6e312bd2add6bab449f0b8.
    
    * fix: make DB_TYPE a const
    
    * fix: update links
    
    * fix: move ioredis to dependencies, update @types/ioredis
    
    * fix: add a comment about nullish coalescing
    
    * fix: move done from try/catch to finally
    
    * fix: gts fix
    
    * fix: callback type
    
    * style: move cb tests up
    
    * fix: gts
    
    * fix: remove unused code
    
    * fix: update ts-node
    
    * fix: nested cb for cb style call tests
    
    * fix: prevent span leaking with better if statement, add else
    
    * fix: remove unneeded || undefined
    
    * fix: ioredis as dev dependency
    
    * fix: do not return a new promise, end span in .then handler
    
    * fix: if statement
    
    * fix: guard against args not being an array
    
    * fix: tests
    
    * Update packages/opentelemetry-plugin-ioredis/README.md
    
    Reword sentence to read better
    
    Co-Authored-By: Daniel Dyla <dyladan@users.noreply.github.com>
    
    * docs: remove sentence from Usage section
    
    * fix: remove unecessary check, rename functions
    
    * fix: rename functions
    
    * fix: port type is number
    
    * docs: add link to available options
    
    * fix: always convert string port to number
    
    * fix: improve inherit options type from ioredis types
    
    * fix: options always exists
    
    * fix: options is always there and all we need
    
    * test: streamify scanning
    
    * test: lua
    
    * test: cb tests were flawed
    
    * fix: trace cb invocations properly
    
    * refactor: same code handles cb and promise style invocations
    
    * fix: update packages
    
    * fix: db statement format
    
    * test: add pipeline test
    
    * fix: allow arguments to be greater than 1
    
    * fix: gts
    
    * test: trace client connection/disconnection
    
    * feat: trace connection
    
    * test: pubsub
    
    * fix: quit instead of disconnect
    
    * fix: update packages
    
    * feat: use test-utils package
    
    Co-authored-by: Daniel Dyla <dyladan@users.noreply.github.com>
    Co-authored-by: Olivier Albertini <olivier.albertini@montreal.ca>

---
+++ packages/opentelemetry-plugin-ioredis/src/enums.ts
@@ -0,0 +1,32 @@
+/*!
+ * Copyright 2019, OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export enum AttributeNames {
+  // required by https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/data-semantic-conventions.md#databases-client-calls
+  COMPONENT = 'component',
+  DB_TYPE = 'db.type',
+  DB_INSTANCE = 'db.instance',
+  DB_STATEMENT = 'db.statement',
+  PEER_ADDRESS = 'peer.address',
+  PEER_HOSTNAME = 'peer.host',
+
+  // optional
+  DB_USER = 'db.user',
+  PEER_PORT = 'peer.port',
+  PEER_IPV4 = 'peer.ipv4',
+  PEER_IPV6 = 'peer.ipv6',
+  PEER_SERVICE = 'peer.service',
+}
