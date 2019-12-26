40ab669f6c1855a82bed48cbb5887a886c68b8b3
Daniel Dyla <dyladan@users.noreply.github.com>
2019-12-26 09:10:08 -0500

    chore: undo changes to basic tracer example

--- examples/basic-tracer-node/index.js
+++ examples/basic-tracer-node/index.js
@@ -2,12 +2,10 @@ const opentelemetry = require('@opentelemetry/core');
 const { BasicTracer, SimpleSpanProcessor } = require('@opentelemetry/tracing');
 const { JaegerExporter } = require('@opentelemetry/exporter-jaeger');
 const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
-const { StackdriverTraceExporter } = require('@opentelemetry/exporter-stackdriver-trace');
-const { CollectorExporter } = require('@opentelemetry/exporter-collector');
+const { CollectorExporter } =  require('@opentelemetry/exporter-collector');
 
 const options = {
-  serviceName: 'basic-service',
-  logger: new opentelemetry.ConsoleLogger(),
+  serviceName: 'basic-service'
 };
 
 // Initialize an exporter depending on how we were started
@@ -18,11 +16,6 @@ if (EXPORTER.toLowerCase().startsWith('z')) {
   exporter = new ZipkinExporter(options);
 } else if (EXPORTER.toLowerCase().startsWith('j')) {
   exporter = new JaegerExporter(options);
-} else if (EXPORTER.toLowerCase().startsWith('s')) {
-  exporter = new StackdriverTraceExporter(Object.assign({}, options, {
-    keyFile: './service_account_key.json',
-    projectId: require('./service_account_key.json').project_id
-  }));
 } else {
   exporter = new CollectorExporter(options);
 }
@@ -38,7 +31,7 @@ opentelemetry.initGlobalTracer(tracer);
 // Create a span. A span must be closed.
 const span = opentelemetry.getTracer().startSpan('main');
 for (let i = 0; i < 10; i++) {
-  doWork(span, i);
+  doWork(span);
 }
 // Be sure to end the span.
 span.end();
@@ -46,22 +39,18 @@ span.end();
 // flush and close the connection.
 exporter.shutdown();
 
-function doWork(parent, index) {
+function doWork(parent) {
   // Start another span. In this example, the main method already started a
   // span, so that'll be the parent span, and this will be a child span.
   const span = opentelemetry.getTracer().startSpan('doWork', {
     parent: parent
   });
 
-  const countTo = Math.random() * 40000000;
-
   // simulate some random work.
-  for (let i = 0; i <= Math.floor(countTo); i++) { }
+  for (let i = 0; i <= Math.floor(Math.random() * 40000000); i++) { }
 
   // Set attributes to the span.
-  span.setAttribute('index', index);
-  span.setAttribute('count to', countTo);
-  span.setAttribute('invald', { object: 'not allowed' })
+  span.setAttribute('key', 'value');
 
   // Annotate our span to capture metadata about our operation
   span.addEvent('invoking doWork').end();
