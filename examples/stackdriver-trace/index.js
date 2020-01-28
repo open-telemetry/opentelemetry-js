eac5e4c1596f08e896ace3b0ac550d57885e7e46
Mayur Kale <mayurkale@google.com>
2020-01-28 12:43:37 -0800

    fix: example lints

--- examples/stackdriver-trace/index.js
+++ examples/stackdriver-trace/index.js
@@ -7,7 +7,7 @@ const { StackdriverTraceExporter } = require('@opentelemetry/exporter-stackdrive
 
 // Initialize an exporter
 const exporter = new StackdriverTraceExporter({
-  projectId: '<PROJECT_ID>'
+  projectId: '<PROJECT_ID>',
 });
 
 const registry = new BasicTracerRegistry();
@@ -17,12 +17,12 @@ registry.addSpanProcessor(new SimpleSpanProcessor(exporter));
 
 // Initialize the OpenTelemetry APIs to use the BasicTracerRegistry bindings
 opentelemetry.initGlobalTracerRegistry(registry);
-const tracer = opentelemetry.getTracer('stackdriver-basic')
+const tracer = opentelemetry.getTracer('stackdriver-basic');
 
 // Create a span. A span must be closed.
 const root = tracer.startSpan('main');
 const related = tracer.startSpan('related', {
-  links: [{ spanContext: root.context() }]
+  links: [{ spanContext: root.context() }],
 });
 
 for (let i = 0; i < 10; i += 1) {
@@ -42,9 +42,7 @@ exporter.shutdown();
 function doWork(parent) {
   // Start another span. In this example, the main method already started a
   // span, so that'll be the parent span, and this will be a child span.
-  const span = tracer.startSpan('doWork', {
-    parent: parent
-  });
+  const span = tracer.startSpan('doWork', { parent });
 
   // simulate some random work.
   const work = Math.floor(Math.random() * 40000000);
