241414845d0cd552411af1f53ad8b550cad0f1ef
Gerhard Stoebich <18708370+Flarna@users.noreply.github.com>
2020-02-12 21:13:50 +0100

    chore: update jsdoc, use single RegEx

--- packages/opentelemetry-core/src/internal/validators.ts
+++ packages/opentelemetry-core/src/internal/validators.ts
@@ -15,9 +15,10 @@
  */
 
 const VALID_KEY_CHAR_RANGE = '[_0-9a-z-*/]';
-const VALID_KEY_REGEX = new RegExp(`^[a-z]${VALID_KEY_CHAR_RANGE}{0,255}$`);
-const VALID_KEY_VENDOR_REGEX = new RegExp(
-  `^[a-z0-9]${VALID_KEY_CHAR_RANGE}{0,240}@[a-z]${VALID_KEY_CHAR_RANGE}{0,13}$`
+const VALID_KEY = `[a-z]${VALID_KEY_CHAR_RANGE}{0,255}`;
+const VALID_VENDOR_KEY = `[a-z0-9]${VALID_KEY_CHAR_RANGE}{0,240}@[a-z]${VALID_KEY_CHAR_RANGE}{0,13}`;
+const VALID_KEY_REGEX = new RegExp(
+  `^(?:${VALID_KEY})|(?:${VALID_VENDOR_KEY})$`
 );
 const VALID_VALUE_BASE_REGEX = /^[ -~]{0,255}[!-~]$/;
 const INVALID_VALUE_COMMA_EQUAL_REGEX = /,|=/;
@@ -26,9 +27,12 @@ const INVALID_VALUE_COMMA_EQUAL_REGEX = /,|=/;
  * Key is opaque string up to 256 characters printable. It MUST begin with a
  * lowercase letter, and can only contain lowercase letters a-z, digits 0-9,
  * underscores _, dashes -, asterisks *, and forward slashes /.
+ * For multi-tenant vendor scenarios, an at sign (@) can be used to prefix the
+ * vendor name. Vendors SHOULD set the tenant ID at the beginning of the key.
+ * see https://www.w3.org/TR/trace-context/#key
  */
 export function validateKey(key: string): boolean {
-  return VALID_KEY_REGEX.test(key) || VALID_KEY_VENDOR_REGEX.test(key);
+  return VALID_KEY_REGEX.test(key);
 }
 
 /**
