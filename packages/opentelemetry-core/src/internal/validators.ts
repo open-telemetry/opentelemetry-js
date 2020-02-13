511e1be0001694de7ce0d6199fe2e70ae22a7b03
Gerhard Stoebich <18708370+Flarna@users.noreply.github.com>
2020-02-13 12:45:06 +0100

    chore: fix regex

--- packages/opentelemetry-core/src/internal/validators.ts
+++ packages/opentelemetry-core/src/internal/validators.ts
@@ -17,9 +17,7 @@
 const VALID_KEY_CHAR_RANGE = '[_0-9a-z-*/]';
 const VALID_KEY = `[a-z]${VALID_KEY_CHAR_RANGE}{0,255}`;
 const VALID_VENDOR_KEY = `[a-z0-9]${VALID_KEY_CHAR_RANGE}{0,240}@[a-z]${VALID_KEY_CHAR_RANGE}{0,13}`;
-const VALID_KEY_REGEX = new RegExp(
-  `^(?:${VALID_KEY})|(?:${VALID_VENDOR_KEY})$`
-);
+const VALID_KEY_REGEX = new RegExp(`^(?:${VALID_KEY}|${VALID_VENDOR_KEY})$`);
 const VALID_VALUE_BASE_REGEX = /^[ -~]{0,255}[!-~]$/;
 const INVALID_VALUE_COMMA_EQUAL_REGEX = /,|=/;
 
