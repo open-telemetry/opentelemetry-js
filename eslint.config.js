dad37af38aad7918a2b51e065d68255fbc92e85d
Olivier Albertini <olivier.albertini@montreal.ca>
2020-04-20 15:21:37 -0400

    fix(eslint): disable ban-ts-ignore only for tests

--- eslint.config.js
+++ eslint.config.js
@@ -27,6 +27,7 @@ module.exports = {
       "files": ["test/**/*.ts"],
       "rules": {
         "no-empty": "off",
+        "@typescript-eslint/ban-ts-ignore": "off",
         "@typescript-eslint/no-empty-function": "off", 
         "@typescript-eslint/no-explicit-any": "off",
         "@typescript-eslint/no-unused-vars": "off",
