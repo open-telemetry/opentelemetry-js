15a1cf7b6781d5831843af97ef4a48a7558635a9
Alan Storm <astorm@alanstorm.com>
2020-05-08 11:47:16 -0700

    reverting "getValues" changes

--- packages/opentelemetry-exporter-prometheus/src/prometheus.ts
+++ packages/opentelemetry-exporter-prometheus/src/prometheus.ts
@@ -184,9 +184,7 @@ export class PrometheusExporter implements MetricExporter {
      */
     if (metric instanceof Counter) {
       metric.remove(
-        ...record.descriptor.labelKeys.map(k =>
-          record.labels[k] ? record.labels[k].toString() : ''
-        )
+        ...record.descriptor.labelKeys.map(k => record.labels[k].toString())
       );
     }
 
