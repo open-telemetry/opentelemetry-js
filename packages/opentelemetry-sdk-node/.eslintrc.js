5d05108620a52d472ecf314a0534ca07405b485b
Daniel Dyla <dyladan@users.noreply.github.com>
2020-06-11 16:07:59 -0400

    chore: set eslint environment

--- packages/opentelemetry-sdk-node/.eslintrc.js
+++ packages/opentelemetry-sdk-node/.eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
     "env": {
         "mocha": true,
-        "commonjs": true,
-        "shared-node-browser": true
+        "node": true
     },
     ...require('../../eslint.config.js')
 }
