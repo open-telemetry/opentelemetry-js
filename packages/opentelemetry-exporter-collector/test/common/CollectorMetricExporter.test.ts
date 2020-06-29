7ce4a77e9a611ad22c0cdf1f8624b6e3444f243d
davidwitten <dwitt12345@gmail.com>
2020-06-29 21:42:43 +0000

    Metric Base

---
+++ packages/opentelemetry-exporter-collector/test/common/CollectorMetricExporter.test.ts
@@ -0,0 +1,176 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ExportResult, NoopLogger } from '@opentelemetry/core';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import { CollectorMetricExporterBase } from '../../src/CollectorMetricExporterBase';
+import { CollectorExporterConfigBase } from '../../src/types';
+import { MetricRecord } from '@opentelemetry/metrics';
+import { mockCounter, mockObserver } from '../helper';
+
+type CollectorExporterConfig = CollectorExporterConfigBase;
+class CollectorMetricExporter extends CollectorMetricExporterBase<
+  CollectorExporterConfig
+> {
+  onInit() {}
+  onShutdown() {}
+  sendMetrics() {}
+  getDefaultUrl(url: string) {
+    return url || '';
+  }
+}
+
+describe('CollectorMetricExporter - common', () => {
+  let collectorExporter: CollectorMetricExporter;
+  let collectorExporterConfig: CollectorExporterConfig;
+  let metrics: MetricRecord[];
+  describe('constructor', () => {
+    let onInitSpy: any;
+
+    beforeEach(() => {
+      onInitSpy = sinon.stub(CollectorMetricExporter.prototype, 'onInit');
+      collectorExporterConfig = {
+        hostName: 'foo',
+        logger: new NoopLogger(),
+        serviceName: 'bar',
+        attributes: {},
+        url: 'http://foo.bar.com',
+      };
+      collectorExporter = new CollectorMetricExporter(collectorExporterConfig);
+      metrics = [];
+      metrics.push(Object.assign({}, mockCounter));
+      metrics.push(Object.assign({}, mockObserver));
+    });
+
+    afterEach(() => {
+      onInitSpy.restore();
+    });
+
+    it('should create an instance', () => {
+      assert.ok(typeof collectorExporter !== 'undefined');
+    });
+
+    it('should call onInit', () => {
+      assert.strictEqual(onInitSpy.callCount, 1);
+    });
+
+    describe('when config contains certain params', () => {
+      it('should set hostName', () => {
+        assert.strictEqual(collectorExporter.hostName, 'foo');
+      });
+
+      it('should set serviceName', () => {
+        assert.strictEqual(collectorExporter.serviceName, 'bar');
+      });
+
+      it('should set url', () => {
+        assert.strictEqual(collectorExporter.url, 'http://foo.bar.com');
+      });
+
+      it('should set logger', () => {
+        assert.ok(collectorExporter.logger === collectorExporterConfig.logger);
+      });
+    });
+
+    describe('when config is missing certain params', () => {
+      beforeEach(() => {
+        collectorExporter = new CollectorMetricExporter();
+      });
+
+      it('should set default serviceName', () => {
+        assert.strictEqual(
+          collectorExporter.serviceName,
+          'collector-metric-exporter'
+        );
+      });
+
+      it('should set default logger', () => {
+        assert.ok(collectorExporter.logger instanceof NoopLogger);
+      });
+    });
+  });
+
+  describe('export', () => {
+    let spySend: any;
+    beforeEach(() => {
+      spySend = sinon.stub(CollectorMetricExporter.prototype, 'sendMetrics');
+      collectorExporter = new CollectorMetricExporter(collectorExporterConfig);
+    });
+    afterEach(() => {
+      spySend.restore();
+    });
+
+    it('should export metrics as collectorTypes.Metrics', done => {
+      collectorExporter.export(metrics, () => {});
+      setTimeout(() => {
+        const metric1 = spySend.args[0][0][0] as MetricRecord;
+        assert.deepStrictEqual(metrics[0], metric1);
+        const metric2 = spySend.args[0][0][1] as MetricRecord;
+        assert.deepStrictEqual(metrics[1], metric2);
+        done();
+      });
+      assert.strictEqual(spySend.callCount, 1);
+    });
+
+    describe('when exporter is shutdown', () => {
+      it('should not export anything but return callback with code "FailedNotRetryable"', () => {
+        collectorExporter.shutdown();
+        spySend.resetHistory();
+
+        const callbackSpy = sinon.spy();
+        collectorExporter.export(metrics, callbackSpy);
+        const returnCode = callbackSpy.args[0][0];
+
+        assert.strictEqual(
+          returnCode,
+          ExportResult.FAILED_NOT_RETRYABLE,
+          'return value is wrong'
+        );
+        assert.strictEqual(spySend.callCount, 0, 'should not call send');
+      });
+    });
+  });
+
+  describe('shutdown', () => {
+    let onShutdownSpy: any;
+    beforeEach(() => {
+      onShutdownSpy = sinon.stub(
+        CollectorMetricExporter.prototype,
+        'onShutdown'
+      );
+      collectorExporterConfig = {
+        hostName: 'foo',
+        logger: new NoopLogger(),
+        serviceName: 'bar',
+        attributes: {},
+        url: 'http://foo.bar.com',
+      };
+      collectorExporter = new CollectorMetricExporter(collectorExporterConfig);
+    });
+    afterEach(() => {
+      onShutdownSpy.restore();
+    });
+
+    it('should call onShutdown', done => {
+      collectorExporter.shutdown();
+      setTimeout(() => {
+        assert.equal(onShutdownSpy.callCount, 1);
+        done();
+      });
+    });
+  });
+});
