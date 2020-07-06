f73aa1ed3c061904a9d8d3718a59de777cd4fbf9
Bartlomiej Obecny <bobecny@gmail.com>
2020-07-06 20:35:25 +0200

    chore: merge branch 'master' into collector_json

--- packages/opentelemetry-exporter-collector/src/platform/node/utilWithGrpc.ts
+++ packages/opentelemetry-exporter-collector/src/platform/node/utilWithGrpc.ts
@@ -79,7 +79,7 @@ export function sendSpansUsingGrpc(
       exportTraceServiceRequest,
       collector.metadata,
       (
-        err: collectorTypes.opentelemetryProto.collector.trace.v1.ExportTraceServiceError
+        err: collectorTypes.ExportServiceError
       ) => {
         if (err) {
           collector.logger.error(
