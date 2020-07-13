cf100b39353ce982a018ca915613a6de9675fa14
davidwitten <dwitt12345@gmail.com>
2020-07-13 19:20:16 +0000

    new line

--- packages/opentelemetry-exporter-collector/src/platform/browser/CollectorMetricExporter.ts
+++ packages/opentelemetry-exporter-collector/src/platform/browser/CollectorMetricExporter.ts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import { MetricRecord } from '@opentelemetry/metrics';
 import { CollectorMetricExporterBase } from '../../CollectorMetricExporterBase';
 import { toCollectorExportMetricServiceRequest } from '../../transformMetrics';
