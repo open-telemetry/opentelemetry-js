d9a1b54f9b61dbc49e3700c4ca9d7ba6d97e6bd2
EdZou <1018766866@qq.com>
2020-08-04 12:36:03 -0500

    feat: added AWS Beanstalk Plugins Resource Detector

---
+++ packages/opentelemetry-resources/src/platform/node/detectors/AwsBeanstalkDetector.ts
@@ -0,0 +1,52 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Resource } from '../../../Resource';
+import { SERVICE_RESOURCE } from '../../../constants';
+import { Detector } from '../../../types';
+import { ResourceDetectionConfigWithLogger } from '../../../config';
+import * as fs from 'fs';
+
+/**
+ * The AwsBeanstalkDetector can be used to detect if a process is running in AWS Elastic
+ * Beanstalk and return a {@link Resource} populated with data about the beanstalk
+ * plugins of AWS X-Ray. Returns an empty Resource if detection fails.
+ *
+ * See https://docs.amazonaws.cn/en_us/xray/latest/devguide/xray-guide.pdf
+ * for more details about detecting information of Elastic Beanstalk plugins
+ */
+class AwsBeanstalkDetector implements Detector {
+  readonly BEANSTALK_CONF_PATH = '/var/elasticbeanstalk/xray/environment.conf';
+
+  async detect(config: ResourceDetectionConfigWithLogger): Promise<Resource> {
+    try {
+      const rawData = fs.readFileSync(this.BEANSTALK_CONF_PATH, 'utf8');
+      const parsedData = JSON.parse(rawData);
+
+      return new Resource({
+        [SERVICE_RESOURCE.NAME]: 'elastic_beanstalk',
+        [SERVICE_RESOURCE.NAMESPACE]: parsedData.environment_name,
+        [SERVICE_RESOURCE.VERSION]: parsedData.version_label,
+        [SERVICE_RESOURCE.INSTANCE_ID]: parsedData.deployment_id,
+      });
+    } catch (e) {
+      config.logger.debug(`AwsEc2Detector failed: ${e.message}`);
+      return Resource.empty();
+    }
+  }
+}
+
+export const awsBeanstalkDetector = new AwsBeanstalkDetector();
---
+++ packages/opentelemetry-resources/test/detectors/AwsBeanstalkDetector.test.ts
@@ -0,0 +1,101 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+import { awsBeanstalkDetector } from '../../src/platform/node/detectors/AwsBeanstalkDetector';
+import {
+  assertEmptyResource,
+  assertServiceResource,
+} from '../util/resource-assertions';
+import { NoopLogger } from '@opentelemetry/core';
+import * as fs from 'fs';
+
+describe('BeanstalkResourceDetector', () => {
+  const err = new Error('failed to load file');
+  const data = {
+    version_label: 'app-5a56-170119_190650-stage-170119_190650',
+    deployment_id: '32',
+    environment_name: 'scorekeep',
+  };
+  const noisyData = {
+    noise: 'noise',
+    version_label: 'app-5a56-170119_190650-stage-170119_190650',
+    deployment_id: '32',
+    environment_name: 'scorekeep',
+  };
+
+  let readStub;
+  let sandbox: sinon.SinonSandbox;
+
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
+  it('should successfully return resource data', async () => {
+    readStub = sandbox.stub(fs, 'readFileSync').returns(JSON.stringify(data));
+    sandbox.stub(JSON, 'parse').returns(data);
+
+    const resource = await awsBeanstalkDetector.detect({
+      logger: new NoopLogger(),
+    });
+
+    sandbox.assert.calledOnce(readStub);
+    assert.ok(resource);
+    assertServiceResource(resource, {
+      name: 'elastic_beanstalk',
+      namespace: 'scorekeep',
+      version: 'app-5a56-170119_190650-stage-170119_190650',
+      instanceId: '32',
+    });
+  });
+
+  it('should successfully return resource data with noise', async () => {
+    readStub = sandbox
+      .stub(fs, 'readFileSync')
+      .returns(JSON.stringify(noisyData));
+    sandbox.stub(JSON, 'parse').returns(noisyData);
+
+    const resource = await awsBeanstalkDetector.detect({
+      logger: new NoopLogger(),
+    });
+
+    sandbox.assert.calledOnce(readStub);
+    assert.ok(resource);
+    assertServiceResource(resource, {
+      name: 'elastic_beanstalk',
+      namespace: 'scorekeep',
+      version: 'app-5a56-170119_190650-stage-170119_190650',
+      instanceId: '32',
+    });
+  });
+
+  it('should return empty resource when failing to read file', async () => {
+    readStub = sandbox.stub(fs, 'readFileSync').throws(err);
+
+    const resource = await awsBeanstalkDetector.detect({
+      logger: new NoopLogger(),
+    });
+
+    sandbox.assert.calledOnce(readStub);
+    assert.ok(resource);
+    assertEmptyResource(resource);
+  });
+});
