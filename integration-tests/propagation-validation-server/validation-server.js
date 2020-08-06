5e4e56c8b75515368bf3c2ed5b31716d094d064f
Steven <srjames@lightstep.com>
2020-08-06 17:58:45 -0400

    refactor: use AsyncHooksContextManager

--- integration-tests/propagation-validation-server/validation-server.js
+++ integration-tests/propagation-validation-server/validation-server.js
@@ -1,15 +1,17 @@
 const axios = require("axios");
-const {
-  HttpTraceContext,
-  getExtractedSpanContext,
-  setExtractedSpanContext,
-} = require("@opentelemetry/core");
+const { HttpTraceContext } = require("@opentelemetry/core");
 const { BasicTracerProvider } = require("@opentelemetry/tracing");
-const { defaultSetter, propagation, trace } = require("@opentelemetry/api");
+const { context, propagation, trace } = require("@opentelemetry/api");
+const {
+  AsyncHooksContextManager,
+} = require("@opentelemetry/context-async-hooks");
 const bodyParser = require("body-parser");
 
 // set global propagator
 propagation.setGlobalPropagator(new HttpTraceContext());
+
+// set global context manager
+context.setGlobalContextManager(new AsyncHooksContextManager());
 
 // Create a provider for activating and tracking spans
 const tracerProvider = new BasicTracerProvider({});
@@ -29,39 +31,35 @@ app.use(bodyParser.json());
 
 // Mount our demo route
 app.post("/verify-tracecontext", (req, res) => {
-  const context = propagation.extract(req.headers);
-  const spanContext = getExtractedSpanContext(context);
-  Promise.all(
-    req.body.map((action) => {
-      const span = tracer.startSpan(
-        "propagate-w3c",
-        { parent: spanContext },
-        context
-      );
-      const headers = {};
-      propagation.inject(
-        headers,
-        defaultSetter,
-        setExtractedSpanContext(context, span.context())
-      );
-      return axios
-        .post(
-          action.url,
+  context.with(propagation.extract(req.headers), () => {
+    Promise.all(
+      req.body.map((action) => {
+        const span = tracer.startSpan("propagate-w3c");
+        let promise;
+        tracer.withSpan(span, () => {
+          const headers = {};
+          propagation.inject(headers);
+          promise = axios
+            .post(
+              action.url,
 
-          [...action.arguments],
+              [...action.arguments],
 
-          {
-            headers,
-            timeout: 10,
-          }
-        )
-        .finally(() => {
-          span.end();
+              {
+                headers,
+                timeout: 10,
+              }
+            )
+            .finally(() => {
+              span.end();
+            });
         });
-    })
-  )
-    .then(() => res.send("hello"))
-    .catch((err) => res.status(500).send(err));
+        return promise;
+      })
+    )
+      .then(() => res.send("hello"))
+      .catch((err) => res.status(500).send(err));
+  });
 });
 
 // Start the server
