2211806c4bef5f40b4d640f9998f23f6a6f8d684
Mark Wolff <mark.wolff@microsoft.com>
2020-08-19 13:19:09 -0700

    fix: address pr comments

--- backwards-compatability/node8/index.ts
+++ backwards-compatability/node8/index.ts
@@ -1,6 +1,10 @@
 import {NodeSDK, api} from '@opentelemetry/sdk-node';
+import {ConsoleSpanExporter} from '@opentelemetry/tracing';
 
-const sdk = new NodeSDK();
+const sdk = new NodeSDK({
+  traceExporter: new ConsoleSpanExporter(),
+  autoDetectResources: false,
+});
 sdk.start();
 
 api.trace.getTracer('test');
