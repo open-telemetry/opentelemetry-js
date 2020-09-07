8718dc96a9b787684fc3de18dc70136c17821d8c
Julien Fabre <julien.fabre.33@gmail.com>
2020-09-07 18:51:49 +0200

    fix: new version with TextMapPropagator interface (#196)
    Co-authored-by: Bartlomiej Obecny <bobecny@gmail.com>

--- packages/opentelemetry-propagator-jaeger/src/JaegerHttpTracePropagator.ts
+++ packages/opentelemetry-propagator-jaeger/src/JaegerHttpTracePropagator.ts
@@ -16,7 +16,7 @@
 
 import {
   Context,
-  HttpTextPropagator,
+  TextMapPropagator,
   SpanContext,
   TraceFlags,
   SetterFunction,
@@ -44,7 +44,7 @@ export const UBER_TRACE_ID_HEADER = 'uber-trace-id';
  * One byte bitmap, as two hex digits.
  * Inspired by jaeger-client-node project.
  */
-export class JaegerHttpTracePropagator implements HttpTextPropagator {
+export class JaegerHttpTracePropagator implements TextMapPropagator {
   private readonly _jaegerTraceHeader: string;
 
   /**
