9469db5a2be910ef713af0e0ca5c02ecd2a174ba
Steve Flanders <sflanders@splunk.com>
2020-09-09 16:16:11 -0400

    Remove non-inclusive language from tests (#1500)
    Co-authored-by: Daniel Dyla <dyladan@users.noreply.github.com>

--- packages/opentelemetry-plugin-xml-http-request/test/xhr.test.ts
+++ packages/opentelemetry-plugin-xml-http-request/test/xhr.test.ts
@@ -112,14 +112,14 @@ function createResource(resource = {}): PerformanceResourceTiming {
   ) as PerformanceResourceTiming;
 }
 
-function createMasterResource(resource = {}): PerformanceResourceTiming {
-  const masterResource: any = createResource(resource);
-  Object.keys(masterResource).forEach((key: string) => {
-    if (typeof masterResource[key] === 'number') {
-      masterResource[key] = masterResource[key] + 30;
+function createMainResource(resource = {}): PerformanceResourceTiming {
+  const mainResource: any = createResource(resource);
+  Object.keys(mainResource).forEach((key: string) => {
+    if (typeof mainResource[key] === 'number') {
+      mainResource[key] = mainResource[key] + 30;
     }
   });
-  return masterResource;
+  return mainResource;
 }
 
 describe('xhr', () => {
@@ -180,7 +180,7 @@ describe('xhr', () => {
             createResource({
               name: fileUrl,
             }),
-            createMasterResource({
+            createMainResource({
               name: fileUrl,
             })
           );
