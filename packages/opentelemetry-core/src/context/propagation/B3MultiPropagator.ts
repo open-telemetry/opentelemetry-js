effcdf32c5f9680e0d545c54b0e389789ae01271
Matthew Wear <matthew.wear@gmail.com>
2020-09-29 18:03:12 -0700

    refactor: remove duplication by using helpers from span-context-utils

--- packages/opentelemetry-core/src/context/propagation/B3MultiPropagator.ts
+++ packages/opentelemetry-core/src/context/propagation/B3MultiPropagator.ts
@@ -23,6 +23,11 @@ import {
   createContextKey,
 } from '@opentelemetry/api';
 import { getParentSpanContext, setExtractedSpanContext } from '../context';
+import {
+  isValidSpanId,
+  isValidTraceId,
+  isSpanContextValid,
+} from '@opentelemetry/api/src/trace/spancontext-utils';
 
 export const X_B3_TRACE_ID = 'x-b3-traceid';
 export const X_B3_SPAN_ID = 'x-b3-spanid';
@@ -32,19 +37,9 @@ export const X_B3_FLAGS = 'x-b3-flags';
 export const DEBUG_FLAG_KEY = createContextKey(
   'OpenTelemetry Context Key B3 Debug Flag'
 );
-const VALID_TRACEID_REGEX = /^([0-9a-f]{16}){1,2}$/i;
-const VALID_SPANID_REGEX = /^[0-9a-f]{16}$/i;
-const INVALID_ID_REGEX = /^0+$/i;
+
 const VALID_SAMPLED_VALUES = new Set([true, 'true', 'True', '1', 1]);
 const VALID_UNSAMPLED_VALUES = new Set([false, 'false', 'False', '0', 0]);
-
-function isValidTraceId(traceId: string): boolean {
-  return VALID_TRACEID_REGEX.test(traceId) && !INVALID_ID_REGEX.test(traceId);
-}
-
-function isValidSpanId(spanId: string): boolean {
-  return VALID_SPANID_REGEX.test(spanId) && !INVALID_ID_REGEX.test(spanId);
-}
 
 function isValidSampledValue(sampled: TraceFlags | undefined): boolean {
   return sampled === TraceFlags.SAMPLED || sampled === TraceFlags.NONE;
@@ -106,30 +101,25 @@ function getTraceFlags(
 export class B3MultiPropagator implements TextMapPropagator {
   inject(context: Context, carrier: unknown, setter: SetterFunction) {
     const spanContext = getParentSpanContext(context);
-    if (!spanContext) return;
+    if (!spanContext || !isSpanContextValid(spanContext)) return;
 
-    if (
-      isValidTraceId(spanContext.traceId) &&
-      isValidSpanId(spanContext.spanId)
-    ) {
-      const debug = context.getValue(DEBUG_FLAG_KEY);
-      setter(carrier, X_B3_TRACE_ID, spanContext.traceId);
-      setter(carrier, X_B3_SPAN_ID, spanContext.spanId);
-      // According to the B3 spec, if the debug flag is set,
-      // the sampled flag shouldn't be propagated as well.
-      if (debug === '1') {
-        setter(carrier, X_B3_FLAGS, debug);
-      } else if (spanContext.traceFlags !== undefined) {
-        // We set the header only if there is an existing sampling decision.
-        // Otherwise we will omit it => Absent.
-        setter(
-          carrier,
-          X_B3_SAMPLED,
-          (TraceFlags.SAMPLED & spanContext.traceFlags) === TraceFlags.SAMPLED
-            ? '1'
-            : '0'
-        );
-      }
+    const debug = context.getValue(DEBUG_FLAG_KEY);
+    setter(carrier, X_B3_TRACE_ID, spanContext.traceId);
+    setter(carrier, X_B3_SPAN_ID, spanContext.spanId);
+    // According to the B3 spec, if the debug flag is set,
+    // the sampled flag shouldn't be propagated as well.
+    if (debug === '1') {
+      setter(carrier, X_B3_FLAGS, debug);
+    } else if (spanContext.traceFlags !== undefined) {
+      // We set the header only if there is an existing sampling decision.
+      // Otherwise we will omit it => Absent.
+      setter(
+        carrier,
+        X_B3_SAMPLED,
+        (TraceFlags.SAMPLED & spanContext.traceFlags) === TraceFlags.SAMPLED
+          ? '1'
+          : '0'
+      );
     }
   }
 
