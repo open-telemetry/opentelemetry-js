b12dacd91aeca54294552091a7127076595ece81
AndrewGrachov <drontxl@yandex.ru>
2020-10-30 07:09:57 +0200

    chore: review fixes

--- eslint.config.js
+++ eslint.config.js
@@ -42,8 +42,8 @@ module.exports = {
         "@typescript-eslint/ban-ts-ignore": "off",
         "@typescript-eslint/no-empty-function": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/no-var-requires": "off",
-        "@typescript-eslint/no-unused-vars": "off"
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-var-requires": "off"
       }
     }
   ]
