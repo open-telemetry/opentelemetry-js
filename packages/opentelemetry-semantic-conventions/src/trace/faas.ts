9172dac1a227208b4efd2e4a674e3b207a5a2708
nirsky <nir@aspecto.io>
2020-11-11 15:19:22 +0200

    fix: lint

--- packages/opentelemetry-semantic-conventions/src/trace/faas.ts
+++ packages/opentelemetry-semantic-conventions/src/trace/faas.ts
@@ -19,157 +19,152 @@
  * https://github.com/open-telemetry/opentelemetry-specification/blob/master/specification/trace/semantic_conventions/faas.md
  */
 export const FaasAttribute = {
-    // General attributes
+  // General attributes
 
-    /**
-     * The name of the function being executed.
-     * 
-     * @remarks
-     * Required.
-     */
-    FAAS_NAME: 'faas.name',
-  
-    /**
-     * The unique ID of the function being executed.
-     * For example, in AWS Lambda this field corresponds to the ARN value, in GCP to the URI of the resource, and in Azure to the FunctionDirectory field.
-     * 
-     * @remarks
-     * Required.
-     */
-    FAAS_ID: 'faas.id',
+  /**
+   * The name of the function being executed.
+   *
+   * @remarks
+   * Required.
+   */
+  FAAS_NAME: 'faas.name',
 
-    /**
-     * The version string of the function being executed.
-     * 
-     * @remarks
-     * Optional.
-     */
-    FAAS_VERSION: 'faas.version',
+  /**
+   * The unique ID of the function being executed.
+   * For example, in AWS Lambda this field corresponds to the ARN value, in GCP to the URI of the resource, and in Azure to the FunctionDirectory field.
+   *
+   * @remarks
+   * Required.
+   */
+  FAAS_ID: 'faas.id',
 
-    /**
-     * The execution environment ID as a string.
-     * 
-     * @remarks
-     * Optional.
-     */
-    FAAS_INSTANCE: 'faas.instance',
+  /**
+   * The version string of the function being executed.
+   *
+   * @remarks
+   * Optional.
+   */
+  FAAS_VERSION: 'faas.version',
 
-    /**
-     * Type of the trigger on which the function is executed.
-     * Possible values: datasource, http, pubsub, timer, other
-     * 
-     * @remarks
-     * Required if applicable.
-     */
-    FAAS_TRIGGER: 'faas.trigger',
-  
-    /**
-     * The execution ID of the current function execution.	
-     *
-     * @remarks
-     * Optional.
-     */
-    FAAS_EXECUTION: 'faas.execution',
+  /**
+   * The execution environment ID as a string.
+   *
+   * @remarks
+   * Optional.
+   */
+  FAAS_INSTANCE: 'faas.instance',
 
+  /**
+   * Type of the trigger on which the function is executed.
+   * Possible values: datasource, http, pubsub, timer, other
+   *
+   * @remarks
+   * Required if applicable.
+   */
+  FAAS_TRIGGER: 'faas.trigger',
 
-    // Incoming Invocations
+  /**
+   * The execution ID of the current function execution.
+   *
+   * @remarks
+   * Optional.
+   */
+  FAAS_EXECUTION: 'faas.execution',
 
-    /**
-     * A boolean that is true if the serverless function is executed for the first time (aka cold-start).
-     *
-     * @remarks
-     * Optional.
-     */
-    FAAS_COLD_START: 'faas.coldstart',
+  // Incoming Invocations
 
-        
-    // Outgoing Invocations
-    // This section describes outgoing FaaS invocations as they are reported by a client calling a FaaS instance.
+  /**
+   * A boolean that is true if the serverless function is executed for the first time (aka cold-start).
+   *
+   * @remarks
+   * Optional.
+   */
+  FAAS_COLD_START: 'faas.coldstart',
 
-    /**
-     * The name of the invoked function.
-     * SHOULD be equal to the faas.name resource attribute of the invoked function.
-     *
-     * @remarks
-     * Optional.
-     */
-    FASS_INVOKED_NAME: 'faas.invoked_name',
+  // Outgoing Invocations
+  // This section describes outgoing FaaS invocations as they are reported by a client calling a FaaS instance.
 
-    /**
-     * The cloud provider of the invoked function. 
-     * SHOULD be equal to the cloud.provider resource attribute of the invoked function.
-     *
-     * @remarks
-     * Optional.
-     */
-    FASS_INVOKED_PROVIDER: 'faas.invoked_provider',
+  /**
+   * The name of the invoked function.
+   * SHOULD be equal to the faas.name resource attribute of the invoked function.
+   *
+   * @remarks
+   * Optional.
+   */
+  FASS_INVOKED_NAME: 'faas.invoked_name',
 
-    /**
-     * The cloud region of the invoked function.
-     *  SHOULD be equal to the cloud.region resource attribute of the invoked function.
-     *
-     * @remarks
-     * Optional.
-     */
-    FASS_INVOKED_REGION: 'faas.invoked_region',
+  /**
+   * The cloud provider of the invoked function.
+   * SHOULD be equal to the cloud.provider resource attribute of the invoked function.
+   *
+   * @remarks
+   * Optional.
+   */
+  FASS_INVOKED_PROVIDER: 'faas.invoked_provider',
 
+  /**
+   * The cloud region of the invoked function.
+   *  SHOULD be equal to the cloud.region resource attribute of the invoked function.
+   *
+   * @remarks
+   * Optional.
+   */
+  FASS_INVOKED_REGION: 'faas.invoked_region',
 
-    // Datesource Trigger
+  // Datesource Trigger
 
-    /**
-     * The name of the source on which the triggering operation was performed. 
-     * For example, in Cloud Storage or S3 corresponds to the bucket name, and in Cosmos DB to the database name.	
-     *
-     * @remarks
-     * Required if applicable.
-     */
-    FAAS_DOC_COLLECTION: 'faas.document.collection',
+  /**
+   * The name of the source on which the triggering operation was performed.
+   * For example, in Cloud Storage or S3 corresponds to the bucket name, and in Cosmos DB to the database name.
+   *
+   * @remarks
+   * Required if applicable.
+   */
+  FAAS_DOC_COLLECTION: 'faas.document.collection',
 
-    /**
-     * Describes the type of the operation that was performed on the data.
-     * MUST be one of the following or, if none of the listed values apply, a custom value: insert, edit, delete	
-     *
-     * @remarks
-     * Required if applicable.
-     */
-    FAAS_DOC_OPERATION: 'faas.document.operation',
+  /**
+   * Describes the type of the operation that was performed on the data.
+   * MUST be one of the following or, if none of the listed values apply, a custom value: insert, edit, delete
+   *
+   * @remarks
+   * Required if applicable.
+   */
+  FAAS_DOC_OPERATION: 'faas.document.operation',
 
-    /**
-     * A string containing the time when the data was accessed in the ISO 8601 format expressed in UTC.	
-     *
-     * @remarks
-     * Required if applicable.
-     */
-    FAAS_DOC_TIME: 'faas.document.time',
+  /**
+   * A string containing the time when the data was accessed in the ISO 8601 format expressed in UTC.
+   *
+   * @remarks
+   * Required if applicable.
+   */
+  FAAS_DOC_TIME: 'faas.document.time',
 
-    /**
-     * The document name/table subjected to the operation. 
-     * For example, in Cloud Storage or S3 is the name of the file, and in Cosmos DB the table name.
-     *
-     * @remarks
-     * Optional.
-     */
-    FAAS_DOC_NAME: 'faas.document.name',
+  /**
+   * The document name/table subjected to the operation.
+   * For example, in Cloud Storage or S3 is the name of the file, and in Cosmos DB the table name.
+   *
+   * @remarks
+   * Optional.
+   */
+  FAAS_DOC_NAME: 'faas.document.name',
 
+  // Timer Trigger
 
-    // Timer Trigger
+  /**
+   * A string containing the function invocation time in the ISO 8601 format expressed in UTC.
+   * Example: "2020-01-23T13:47:06Z"
+   *
+   * @remarks
+   * Required if applicable.
+   */
+  FAAS_TIME: 'faas.time',
 
-    /**
-     * A string containing the function invocation time in the ISO 8601 format expressed in UTC.	
-     * Example: "2020-01-23T13:47:06Z"
-     *
-     * @remarks
-     * Required if applicable.
-     */
-    FAAS_TIME: 'faas.time',
-
-    /**
-     * A string containing the schedule period as Cron Expression.	
-     * Example: "0/5 * * * ? *"	
-     *
-     * @remarks
-     * Optional.
-     */
-    FAAS_CRON: 'faas.cron',
+  /**
+   * A string containing the schedule period as Cron Expression.
+   * Example: "0/5 * * * ? *"
+   *
+   * @remarks
+   * Optional.
+   */
+  FAAS_CRON: 'faas.cron',
 };
-  
