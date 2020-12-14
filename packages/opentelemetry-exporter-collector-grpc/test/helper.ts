6302a1668c5a456e530f93e919314ef1eaf53830
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2020-12-14 22:24:29 +0530

    chore: fix lint

--- packages/opentelemetry-exporter-collector-grpc/test/helper.ts
+++ packages/opentelemetry-exporter-collector-grpc/test/helper.ts
@@ -87,9 +87,7 @@ export function mockObserver(
   return metric;
 }
 
-export function mockValueRecorder(): metrics.Metric<
-  metrics.BoundValueRecorder
-> &
+export function mockValueRecorder(): metrics.Metric<metrics.BoundValueRecorder> &
   api.ValueRecorder {
   const name = 'int-recorder';
   const metric =
