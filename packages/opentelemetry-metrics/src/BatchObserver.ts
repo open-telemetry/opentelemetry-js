48b60c980c904e45aea556f0a3177e64d46134a7
legendecas <legendecas@gmail.com>
2020-12-15 04:27:49 +0800

    refactor: batch observer to be independent from metric types (#1709)
    * refactor: batch observer to be independent from metric types
    
    - BatchObservers do not have to be created with names
    - BatchObservers do not have instruments for their own
    
    * fixup: nodejs v8 compatibilities
    
    * fixup: replace all batch observer metric occurrences with batch observer

--- packages/opentelemetry-metrics/src/BatchObserver.ts
+++ packages/opentelemetry-metrics/src/BatchObserver.ts
@@ -15,57 +15,32 @@
  */
 
 import * as api from '@opentelemetry/api';
-import { InstrumentationLibrary } from '@opentelemetry/core';
-import { Resource } from '@opentelemetry/resources';
+import { Logger, NoopLogger } from '@opentelemetry/api';
 import { BatchObserverResult } from './BatchObserverResult';
-import { BoundObserver } from './BoundInstrument';
-import { Processor } from './export/Processor';
-import { MetricKind, MetricRecord } from './export/types';
-import { Metric } from './Metric';
+import { MetricRecord } from './export/types';
 
 const NOOP_CALLBACK = () => {};
 const MAX_TIMEOUT_UPDATE_MS = 500;
 
-/** This is a SDK implementation of Batch Observer Metric. */
-export class BatchObserverMetric
-  extends Metric<BoundObserver>
-  implements api.BatchObserver {
+/** This is a SDK implementation of Batch Observer. */
+export class BatchObserver {
   private _callback: (observerResult: api.BatchObserverResult) => void;
   private _maxTimeoutUpdateMS: number;
+  private _logger: Logger;
 
   constructor(
-    name: string,
-    options: api.BatchMetricOptions,
-    private readonly _processor: Processor,
-    resource: Resource,
-    instrumentationLibrary: InstrumentationLibrary,
+    options: api.BatchObserverOptions,
     callback?: (observerResult: api.BatchObserverResult) => void
   ) {
-    super(
-      name,
-      options,
-      MetricKind.BATCH_OBSERVER,
-      resource,
-      instrumentationLibrary
-    );
+    this._logger = options.logger ?? new NoopLogger();
     this._maxTimeoutUpdateMS =
       options.maxTimeoutUpdateMS ?? MAX_TIMEOUT_UPDATE_MS;
     this._callback = callback || NOOP_CALLBACK;
   }
 
-  protected _makeInstrument(labels: api.Labels): BoundObserver {
-    return new BoundObserver(
-      labels,
-      this._disabled,
-      this._valueType,
-      this._logger,
-      this._processor.aggregatorFor(this._descriptor)
-    );
-  }
-
-  getMetricRecord(): Promise<MetricRecord[]> {
+  collect(): Promise<MetricRecord[]> {
     this._logger.debug('getMetricRecord - start');
-    return new Promise((resolve, reject) => {
+    return new Promise(resolve => {
       const observerResult = new BatchObserverResult();
 
       // cancels after MAX_TIMEOUT_MS - no more waiting for results
@@ -74,14 +49,14 @@ export class BatchObserverMetric
         // remove callback to prevent user from updating the values later if
         // for any reason the observerBatchResult will be referenced
         observerResult.onObserveCalled();
-        super.getMetricRecord().then(resolve, reject);
+        resolve();
         this._logger.debug('getMetricRecord - timeout');
       }, this._maxTimeoutUpdateMS);
 
       // sets callback for each "observe" method
       observerResult.onObserveCalled(() => {
         clearTimeout(timer);
-        super.getMetricRecord().then(resolve, reject);
+        resolve();
         this._logger.debug('getMetricRecord - end');
       });
 
