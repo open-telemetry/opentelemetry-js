61106d33a4d11361993084493113d101fc369fbf
Daniel Radetsky <dradetsky@gmail.com>
2020-12-22 17:36:56 -0800

    bind->bindAsync, but this breaks test

--- packages/opentelemetry-exporter-collector-grpc/test/CollectorMetricExporter.test.ts
+++ packages/opentelemetry-exporter-collector-grpc/test/CollectorMetricExporter.test.ts
@@ -106,9 +106,10 @@ const testCollectorMetricExporter = (params: TestParams) =>
                 ]
               )
             : grpc.ServerCredentials.createInsecure();
-          server.bind(address, credentials);
-          server.start();
-          done();
+          server.bindAsync(address, credentials, () => {
+            server.start();
+            done();
+          });
         });
     });
 
