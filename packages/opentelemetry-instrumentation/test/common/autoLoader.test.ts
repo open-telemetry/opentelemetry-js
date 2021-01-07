89664a7c4e6b58f29ac047f0dc2434201827f12d
Bartlomiej Obecny <bobecny@gmail.com>
2021-01-07 17:03:39 +0100

    feat: creating one auto loader for instrumentation and old plugins (#1731)
    * feat: creating one auto loader for instrumentation and old plugins
    
    * feat: adding temporary tests for autoloader
    
    * chore: adding auto loader for instrumentation and plugins
    
    * chore: removing temporary test
    
    * chore: reverting changes done temporary
    
    * chore: linting
    
    * chore: updating submodule for opentelemetry-proto
    
    * chore: updating submodule for exporter-collector-grpc

---
+++ packages/opentelemetry-instrumentation/test/common/autoLoader.test.ts
@@ -0,0 +1,93 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { NOOP_METER_PROVIDER, NOOP_TRACER_PROVIDER } from '@opentelemetry/api';
+import * as assert from 'assert';
+import * as sinon from 'sinon';
+
+import { InstrumentationBase, registerInstrumentations } from '../../src';
+
+class FooInstrumentation extends InstrumentationBase {
+  init() {
+    return [];
+  }
+  enable() {}
+  disable() {}
+}
+
+describe('autoLoader', () => {
+  let sandbox: sinon.SinonSandbox;
+  let unload: Function | undefined;
+  beforeEach(() => {
+    sandbox = sinon.createSandbox();
+  });
+  afterEach(() => {
+    sandbox.restore();
+    if (typeof unload === 'function') {
+      unload();
+      unload = undefined;
+    }
+  });
+
+  describe('registerInstrumentations', () => {
+    describe('InstrumentationBase', () => {
+      let instrumentation: InstrumentationBase;
+      let enableSpy: sinon.SinonSpy;
+      let setTracerProviderSpy: sinon.SinonSpy;
+      let setsetMeterProvider: sinon.SinonSpy;
+      const tracerProvider = NOOP_TRACER_PROVIDER;
+      const meterProvider = NOOP_METER_PROVIDER;
+      beforeEach(() => {
+        instrumentation = new FooInstrumentation('foo', '1', {});
+        enableSpy = sandbox.spy(instrumentation, 'enable');
+        setTracerProviderSpy = sandbox.stub(
+          instrumentation,
+          'setTracerProvider'
+        );
+        setsetMeterProvider = sandbox.stub(instrumentation, 'setMeterProvider');
+        unload = registerInstrumentations({
+          instrumentations: [instrumentation],
+          tracerProvider,
+          meterProvider,
+        });
+      });
+
+      afterEach(() => {
+        Object.keys(require.cache).forEach(key => delete require.cache[key]);
+        if (typeof unload === 'function') {
+          unload();
+          unload = undefined;
+        }
+      });
+
+      it('should enable instrumentation', () => {
+        assert.strictEqual(enableSpy.callCount, 1);
+      });
+
+      it('should set TracerProvider', () => {
+        assert.strictEqual(setTracerProviderSpy.callCount, 1);
+        assert.ok(setTracerProviderSpy.lastCall.args[0] === tracerProvider);
+        assert.strictEqual(setTracerProviderSpy.lastCall.args.length, 1);
+      });
+
+      it('should set MeterProvider', () => {
+        assert.strictEqual(setsetMeterProvider.callCount, 1);
+        assert.ok(setsetMeterProvider.lastCall.args[0] === meterProvider);
+        assert.strictEqual(setsetMeterProvider.lastCall.args.length, 1);
+      });
+    });
+  });
+});
