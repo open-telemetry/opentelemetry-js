a14e5d8bdb1a8c6e5d02658c6a51ac4761370422
Daniel Dyla <dyladan@users.noreply.github.com>
2021-01-08 16:31:19 -0500

    chore: fix test use of api disable

--- packages/opentelemetry-instrumentation-http/test/functionals/https-enable.test.ts
+++ packages/opentelemetry-instrumentation-http/test/functionals/https-enable.test.ts
@@ -69,7 +69,6 @@ const provider = new BasicTracerProvider({
 instrumentation.setTracerProvider(provider);
 const tracer = provider.getTracer('test-https');
 provider.addSpanProcessor(new SimpleSpanProcessor(memoryExporter));
-propagation.setGlobalPropagator(new DummyPropagation());
 
 function doNock(
   hostname: string,
@@ -94,12 +93,14 @@ describe('HttpsInstrumentation', () => {
 
   beforeEach(() => {
     contextManager = new AsyncHooksContextManager().enable();
+    propagation.setGlobalPropagator(new DummyPropagation());
     context.setGlobalContextManager(contextManager);
   });
 
   afterEach(() => {
     contextManager.disable();
     context.disable();
+    propagation.disable();
   });
 
   describe('enable()', () => {
