dd7718af7f1f939276da0dfd49edc79b555638bd
Daniel Dyla <dyladan@users.noreply.github.com>
2021-01-13 18:03:20 -0500

    chore: remove race from tests (#1814)

--- packages/opentelemetry-exporter-collector-proto/test/CollectorMetricExporter.test.ts
+++ packages/opentelemetry-exporter-collector-proto/test/CollectorMetricExporter.test.ts
@@ -14,30 +14,28 @@
  * limitations under the License.
  */
 
-import {
-  collectorTypes,
-  CollectorExporterNodeConfigBase,
-} from '@opentelemetry/exporter-collector';
 import * as api from '@opentelemetry/api';
+import { ExportResultCode } from '@opentelemetry/core';
+import {
+  CollectorExporterNodeConfigBase,
+  collectorTypes,
+} from '@opentelemetry/exporter-collector';
 import * as metrics from '@opentelemetry/metrics';
-import * as http from 'http';
 import * as assert from 'assert';
+import * as http from 'http';
 import * as sinon from 'sinon';
 import { CollectorMetricExporter } from '../src';
 import { getExportRequestProto } from '../src/util';
-
 import {
-  mockCounter,
-  mockObserver,
-  ensureExportMetricsServiceRequestIsSet,
-  mockValueRecorder,
   ensureExportedCounterIsCorrect,
   ensureExportedObserverIsCorrect,
   ensureExportedValueRecorderIsCorrect,
+  ensureExportMetricsServiceRequestIsSet,
+  mockCounter,
   MockedResponse,
+  mockObserver,
+  mockValueRecorder,
 } from './helper';
-import { ExportResult, ExportResultCode } from '@opentelemetry/core';
-import { CollectorExporterError } from '@opentelemetry/exporter-collector/build/src/types';
 
 const fakeRequest = {
   end: function () {},
@@ -45,19 +43,13 @@ const fakeRequest = {
   write: function () {},
 };
 
-// send is lazy loading file so need to wait a bit
-const waitTimeMS = 20;
-
 describe('CollectorMetricExporter - node with proto over http', () => {
   let collectorExporter: CollectorMetricExporter;
   let collectorExporterConfig: CollectorExporterNodeConfigBase;
-  let spyRequest: sinon.SinonSpy;
-  let spyWrite: sinon.SinonSpy;
   let metrics: metrics.MetricRecord[];
+
   describe('export', () => {
     beforeEach(async () => {
-      spyRequest = sinon.stub(http, 'request').returns(fakeRequest as any);
-      spyWrite = sinon.stub(fakeRequest, 'write');
       collectorExporterConfig = {
         headers: {
           foo: 'bar',
@@ -95,131 +87,120 @@ describe('CollectorMetricExporter - node with proto over http', () => {
       metrics.push((await recorder.getMetricRecord())[0]);
     });
     afterEach(() => {
-      spyRequest.restore();
-      spyWrite.restore();
+      sinon.restore();
     });
 
     it('should open the connection', done => {
       collectorExporter.export(metrics, () => {});
 
-      setTimeout(() => {
-        const args = spyRequest.args[0];
-        const options = args[0];
-
+      sinon.stub(http, 'request').callsFake((options: any) => {
         assert.strictEqual(options.hostname, 'foo.bar.com');
         assert.strictEqual(options.method, 'POST');
         assert.strictEqual(options.path, '/');
         done();
-      }, waitTimeMS);
+        return fakeRequest as any;
+      });
     });
 
     it('should set custom headers', done => {
       collectorExporter.export(metrics, () => {});
 
-      setTimeout(() => {
-        const args = spyRequest.args[0];
-        const options = args[0];
+      sinon.stub(http, 'request').callsFake((options: any) => {
         assert.strictEqual(options.headers['foo'], 'bar');
         done();
-      }, waitTimeMS);
+        return fakeRequest as any;
+      });
     });
 
     it('should have keep alive and keepAliveMsecs option set', done => {
       collectorExporter.export(metrics, () => {});
 
-      setTimeout(() => {
-        const args = spyRequest.args[0];
-        const options = args[0];
-        const agent = options.agent;
-        assert.strictEqual(agent.keepAlive, true);
-        assert.strictEqual(agent.options.keepAliveMsecs, 2000);
+      sinon.stub(http, 'request').callsFake((options: any) => {
+        assert.strictEqual(options.agent.keepAlive, true);
+        assert.strictEqual(options.agent.options.keepAliveMsecs, 2000);
         done();
+        return fakeRequest as any;
       });
     });
 
     it('should successfully send metrics', done => {
       collectorExporter.export(metrics, () => {});
 
-      setTimeout(() => {
-        const writeArgs = spyWrite.args[0];
-        const ExportTraceServiceRequestProto = getExportRequestProto();
-        const data = ExportTraceServiceRequestProto?.decode(writeArgs[0]);
-        const json = data?.toJSON() as collectorTypes.opentelemetryProto.collector.metrics.v1.ExportMetricsServiceRequest;
+      sinon.stub(http, 'request').returns({
+        end: () => {},
+        on: () => {},
+        write: (...writeArgs: any[]) => {
+          const ExportTraceServiceRequestProto = getExportRequestProto();
+          const data = ExportTraceServiceRequestProto?.decode(writeArgs[0]);
+          const json = data?.toJSON() as collectorTypes.opentelemetryProto.collector.metrics.v1.ExportMetricsServiceRequest;
 
-        const metric1 =
-          json.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[0];
-        const metric2 =
-          json.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[1];
-        const metric3 =
-          json.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[2];
+          const metric1 =
+            json.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[0];
+          const metric2 =
+            json.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[1];
+          const metric3 =
+            json.resourceMetrics[0].instrumentationLibraryMetrics[0].metrics[2];
 
-        assert.ok(typeof metric1 !== 'undefined', "counter doesn't exist");
-        ensureExportedCounterIsCorrect(
-          metric1,
-          metric1.intSum?.dataPoints[0].timeUnixNano
-        );
-        assert.ok(typeof metric2 !== 'undefined', "observer doesn't exist");
-        ensureExportedObserverIsCorrect(
-          metric2,
-          metric2.doubleGauge?.dataPoints[0].timeUnixNano
-        );
-        assert.ok(
-          typeof metric3 !== 'undefined',
-          "value recorder doesn't exist"
-        );
-        ensureExportedValueRecorderIsCorrect(
-          metric3,
-          metric3.intHistogram?.dataPoints[0].timeUnixNano,
-          [0, 100],
-          ['0', '2', '0']
-        );
+          assert.ok(typeof metric1 !== 'undefined', "counter doesn't exist");
+          ensureExportedCounterIsCorrect(
+            metric1,
+            metric1.intSum?.dataPoints[0].timeUnixNano
+          );
+          assert.ok(typeof metric2 !== 'undefined', "observer doesn't exist");
+          ensureExportedObserverIsCorrect(
+            metric2,
+            metric2.doubleGauge?.dataPoints[0].timeUnixNano
+          );
+          assert.ok(
+            typeof metric3 !== 'undefined',
+            "value recorder doesn't exist"
+          );
+          ensureExportedValueRecorderIsCorrect(
+            metric3,
+            metric3.intHistogram?.dataPoints[0].timeUnixNano,
+            [0, 100],
+            ['0', '2', '0']
+          );
 
-        ensureExportMetricsServiceRequestIsSet(json);
+          ensureExportMetricsServiceRequestIsSet(json);
 
-        done();
-      }, waitTimeMS);
+          done();
+        },
+      } as any);
     });
 
     it('should log the successful message', done => {
       const spyLoggerError = sinon.stub(collectorExporter.logger, 'error');
 
-      const responseSpy = sinon.spy();
-      collectorExporter.export(metrics, responseSpy);
+      collectorExporter.export(metrics, result => {
+        assert.strictEqual(result.code, ExportResultCode.SUCCESS);
+        assert.strictEqual(spyLoggerError.args.length, 0);
+        done();
+      });
 
-      setTimeout(() => {
+      sinon.stub(http, 'request').callsFake((options: any, cb: any) => {
         const mockRes = new MockedResponse(200);
-        const args = spyRequest.args[0];
-        const callback = args[1];
-        callback(mockRes);
+        cb(mockRes);
         mockRes.send('success');
-        setTimeout(() => {
-          const result = responseSpy.args[0][0] as ExportResult;
-          assert.strictEqual(result.code, ExportResultCode.SUCCESS);
-          assert.strictEqual(spyLoggerError.args.length, 0);
-          done();
-        });
-      }, waitTimeMS);
+        return fakeRequest as any;
+      });
     });
 
     it('should log the error message', done => {
-      const responseSpy = sinon.spy();
-      collectorExporter.export(metrics, responseSpy);
+      collectorExporter.export(metrics, result => {
+        assert.strictEqual(result.code, ExportResultCode.FAILED);
+        // @ts-expect-error
+        assert.strictEqual(result.error.code, 400);
+        done();
+      });
 
-      setTimeout(() => {
-        const mockRes = new MockedResponse(400);
-        const args = spyRequest.args[0];
-        const callback = args[1];
-        callback(mockRes);
-        mockRes.send('failed');
-        setTimeout(() => {
-          const result = responseSpy.args[0][0] as ExportResult;
-          assert.strictEqual(result.code, ExportResultCode.FAILED);
-          const error = result.error as CollectorExporterError;
-          assert.strictEqual(error.code, 400);
-          assert.strictEqual(error.data, 'failed');
-          done();
-        });
-      }, waitTimeMS);
+      sinon.stub(http, 'request').callsFake((options: any, cb: any) => {
+        const mockResError = new MockedResponse(400);
+        cb(mockResError);
+        mockResError.send('failed');
+
+        return fakeRequest as any;
+      });
     });
   });
 });
