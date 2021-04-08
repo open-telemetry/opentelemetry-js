c131413e6c6bd7d3e09bb2317c2f977616427093
Weyert de Boer <weyert.deboer@tapico.io>
2021-04-08 02:10:32 +0100

    feat: add semconv generator for `semantic-conventions`-package
    Adds a build script which generate the files for the `semenatic-conventions`-package
    so it always in sync with the definitions in the `specifications`-repo
    
    A few unspecified attributes are added manually to the semconv template to
    allow existing code to pass, these are:
    
      // HTTP
      HTTP_ERROR_NAME: 'http.error_name',
      HTTP_ERROR_MESSAGE: 'http.error_message',
      HTTP_STATUS_TEXT: 'http.status_text',
    
      // GRPC
      GRPC_KIND: 'grpc.kind', // SERVER or CLIENT
      GRPC_METHOD: 'grpc.method',
      GRPC_STATUS_CODE: 'grpc.status_code',
      GRPC_ERROR_NAME: 'grpc.error_name',
      GRPC_ERROR_MESSAGE: 'grpc.error_message',

---
+++ scripts/semconv/generate.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="${SCRIPT_DIR}/../../"
+
+# freeze the spec version to make SemanticAttributes generation reproducible
+SPEC_VERSION=main
+
+cd ${SCRIPT_DIR}
+
+rm -rf opentelemetry-specification || true
+mkdir opentelemetry-specification
+cd opentelemetry-specification
+
+git init
+git remote add origin https://github.com/open-telemetry/opentelemetry-specification.git
+git fetch origin "$SPEC_VERSION"
+git reset --hard FETCH_HEAD
+cd ${SCRIPT_DIR}
+
+docker run --rm \
+  -v ${SCRIPT_DIR}/opentelemetry-specification/semantic_conventions/trace:/source \
+  -v ${SCRIPT_DIR}/templates:/templates \
+  -v ${ROOT_DIR}/packages/opentelemetry-semantic-conventions/src/trace/:/output \
+  otel/semconvgen \
+  -f /source code \
+  --template /templates/SemanticAttributes.ts.j2 \
+  --output /output/SemanticAttribute.ts \
+  -Dclass=SemanticAttribute
+
+docker run --rm \
+  -v ${SCRIPT_DIR}/opentelemetry-specification/semantic_conventions/resource:/source \
+  -v ${SCRIPT_DIR}/templates:/templates \
+  -v ${ROOT_DIR}/packages/opentelemetry-semantic-conventions/src/resource/:/output \
+  otel/semconvgen \
+  -f /source code \
+  --template /templates/SemanticAttributes.ts.j2 \
+  --output /output/ResourceAttribute.ts \
+  -Dclass=ResourceAttributes
+
+cd "$ROOT_DIR"
