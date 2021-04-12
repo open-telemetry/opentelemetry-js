7bdb2745392810ca2b5cfe2007c8ddfd013f6f44
Anuraag Agrawal <aanuraag@amazon.co.jp>
2021-04-12 15:40:31 +0900

    feat: add AWS Lambda detector

--- packages/opentelemetry-resource-detector-aws/src/detectors/index.ts
+++ packages/opentelemetry-resource-detector-aws/src/detectors/index.ts
@@ -18,3 +18,4 @@ export * from './AwsEc2Detector';
 export * from './AwsBeanstalkDetector';
 export * from './AwsEcsDetector';
 export * from './AwsEksDetector';
+export * from './AwsLambdaDetector';
