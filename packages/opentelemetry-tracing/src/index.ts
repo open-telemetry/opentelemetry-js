1758fa6e84333c9c43cc468c5e99dd3646f7e3c7
Valentin Marchaud <contact@vmarchaud.fr>
2021-05-12 17:58:55 +0200

    feat(tracing): allow to configure exporter by environment #1676 (#2100)
    Co-authored-by: Bartlomiej Obecny <bobecny@gmail.com>
    Co-authored-by: Daniel Dyla <dyladan@users.noreply.github.com>

--- packages/opentelemetry-tracing/src/index.ts
+++ packages/opentelemetry-tracing/src/index.ts
@@ -22,6 +22,7 @@ export * from './export/InMemorySpanExporter';
 export * from './export/ReadableSpan';
 export * from './export/SimpleSpanProcessor';
 export * from './export/SpanExporter';
+export * from './export/NoopSpanProcessor';
 export * from './Span';
 export * from './SpanProcessor';
 export * from './TimedEvent';
