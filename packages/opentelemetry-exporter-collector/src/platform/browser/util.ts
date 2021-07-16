05321473b72d359a231fddee1fab5399b9e98aa6
Nikolaos Achilles <niko.achilles.kokkinos@gmail.com>
2021-07-16 20:00:22 +0300

    fix: review headers are appended to existing one (open-telemetry#2335)

--- packages/opentelemetry-exporter-collector/src/platform/browser/util.ts
+++ packages/opentelemetry-exporter-collector/src/platform/browser/util.ts
@@ -56,11 +56,15 @@ export function sendWithXhr(
   const xhr = new XMLHttpRequest();
   xhr.open('POST', url);
 
-  if (!Object.keys(headers).includes('Accept'))
-    xhr.setRequestHeader('Accept', 'application/json');
-  if (!Object.keys(headers).includes('Content-Type'))
-    xhr.setRequestHeader('Content-Type', 'application/json');
-  Object.entries(headers).forEach(([k, v]) => {
+  const defaultHeaders = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  };
+
+  Object.entries({
+    ...defaultHeaders,
+    ...headers,
+  }).forEach(([k, v]) => {
     xhr.setRequestHeader(k, v);
   });
 
