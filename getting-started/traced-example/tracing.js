open-telemetry/opentelemetry-node

chore: apply lint on getting-started

getting-started/traced-example/tracing.js
@@ -1,18 +1,16 @@
-"use strict";
-
-const { NodeTracerProvider } = require("@opentelemetry/node");
-const { SimpleSpanProcessor } = require("@opentelemetry/tracing");
+const { NodeTracerProvider } = require('@opentelemetry/node');
+const { SimpleSpanProcessor } = require('@opentelemetry/tracing');
 const { Resource } = require('@opentelemetry/resources');
 const { ResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    [ResourceAttributes.SERVICE_NAME]: "getting-started",
-  })
+    [ResourceAttributes.SERVICE_NAME]: 'getting-started',
+  }),
 });
 
 provider.addSpanProcessor(
@@ -21,8 +19,8 @@ provider.addSpanProcessor(
       // If you are running your tracing backend on another host,
       // you can point to it using the `url` parameter of the
       // exporter config.
-    })
-  )
+    }),
+  ),
 );
 
 provider.register();
@@ -35,4 +33,4 @@ registerInstrumentations({
   ],
 });
 
-console.log("tracing initialized");
+console.log('tracing initialized');

getting-started/ts-example/traced-example/tracing.ts
@@ -1,9 +1,10 @@
 import { NodeTracerProvider } from '@opentelemetry/node';
-const { Resource } = require('@opentelemetry/resources');
-const { ResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 import { SimpleSpanProcessor } from '@opentelemetry/tracing';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
+
+const { Resource } = require('@opentelemetry/resources');
+const { ResourceAttributes } = require('@opentelemetry/semantic-conventions');
 // For Jaeger, use the following line instead:
 // import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 
@@ -35,6 +36,5 @@ registerInstrumentations({
     new HttpInstrumentation(),
   ],
 });
-
 
 console.log('tracing initialized');

open-telemetry/opentelemetry-js

getting-started/traced-example/tracing.js
@@ -1,18 +1,16 @@
-"use strict";
-
-const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "getting-started",
-  })
+    [SemanticResourceAttributes.SERVICE_NAME]: 'getting-started',
+  }),
 });
 
 provider.addSpanProcessor(
@@ -21,8 +19,8 @@ provider.addSpanProcessor(
       // If you are running your tracing backend on another host,
       // you can point to it using the `url` parameter of the
       // exporter config.
-    })
-  )
+    }),
+  ),
 );
 
 provider.register();
@@ -35,4 +33,4 @@ registerInstrumentations({
   ],
 });
 
-console.log("tracing initialized");
+console.log('tracing initialized');

getting-started/ts-example/traced-example/tracing.ts
@@ -1,9 +1,10 @@
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
-const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 
 import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
 import { ZipkinExporter } from '@opentelemetry/exporter-zipkin';
+
+const { Resource } = require('@opentelemetry/resources');
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
 // For Jaeger, use the following line instead:
 // import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
 
@@ -35,6 +36,5 @@ registerInstrumentations({
     new HttpInstrumentation(),
   ],
 });
-
 
 console.log('tracing initialized');