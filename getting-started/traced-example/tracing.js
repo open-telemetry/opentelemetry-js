c62ea117a6148a075fe04b2778996f9a0e3ae278
Ali Sabzevari <alisabzevari@gmail.com>
2021-08-23 23:46:10 +0200

    eslint configuration for getting-started examples (#2297)
    * chore: add eslint config to getting-started
    
    * chore: apply lint on getting-started
    
    * chore: fix strict rule in getting-started lint config
    
    * chore: import examples eslintrc for getting-started eslint config
    
    * chore: rearrage lint scripts in package.json
    
    * chore: restore removed require from getting-started example
    
    * chore: remove duplicate require in getting-started

--- getting-started/traced-example/tracing.js
+++ getting-started/traced-example/tracing.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
-const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
-const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
+const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
 const { Resource } = require('@opentelemetry/resources');
 const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
-const { ZipkinExporter } = require("@opentelemetry/exporter-zipkin");
+const { ZipkinExporter } = require('@opentelemetry/exporter-zipkin');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 
 const provider = new NodeTracerProvider({
   resource: new Resource({
-    [SemanticResourceAttributes.SERVICE_NAME]: "getting-started",
-  })
+    [SemanticResourceAttributes.SERVICE_NAME]: 'getting-started',
+  }),
 });
 
 provider.addSpanProcessor(
@@ -21,8 +21,8 @@ provider.addSpanProcessor(
       // If you are running your tracing backend on another host,
       // you can point to it using the `url` parameter of the
       // exporter config.
-    })
-  )
+    }),
+  ),
 );
 
 provider.register();
@@ -35,4 +35,4 @@ registerInstrumentations({
   ],
 });
 
-console.log("tracing initialized");
+console.log('tracing initialized');
