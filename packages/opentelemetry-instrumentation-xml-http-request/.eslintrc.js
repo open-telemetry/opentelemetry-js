07b19ad19e7045d48bbbba1fd8c319066619c256
Daniel Dyla <dyladan@users.noreply.github.com>
2021-09-10 17:14:55 -0400

    chore: split stable and experimental packages into groups using directories (#2462)
    * chore: split stable and experimental packages into groups
    
    * chore: use experimental directory
    
    * chore: remove release-please

--- packages/opentelemetry-instrumentation-xml-http-request/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "env": {
-        "mocha": true,
-        "commonjs": true,
-        "browser": true,
-        "jquery": true
-    },
-    ...require('../../eslint.config.js')
-}
