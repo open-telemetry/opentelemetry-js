4d9f5f2f76bcf6c39b6af01d3d80988fe3e479d2
Daniel Dyla <dyladan@users.noreply.github.com>
2021-09-22 18:53:02 -0400

    chore: fix path

--- experimental/packages/opentelemetry-exporter-otlp-http/karma.conf.js
+++ experimental/packages/opentelemetry-exporter-otlp-http/karma.conf.js
@@ -15,7 +15,7 @@
  */
 
 const karmaWebpackConfig = require('../../../karma.webpack');
-const karmaBaseConfig = require('../../../.base');
+const karmaBaseConfig = require('../../../karma.base');
 
 module.exports = (config) => {
   config.set(Object.assign({}, karmaBaseConfig, {
