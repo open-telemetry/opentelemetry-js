93381714dde65c2ab74b11b025eb5ca04905c3cf
legendecas <legendecas@gmail.com>
2021-11-08 16:57:52 +0800

    fix: align globalThis fallbacks with otel-core (#126)
    Co-authored-by: Valentin Marchaud <contact@vmarchaud.fr>

--- api/src/platform/browser/globalThis.ts
+++ api/src/platform/browser/globalThis.ts
@@ -14,6 +14,22 @@
  * limitations under the License.
  */
 
+// Updates to this file should also be replicated to @opentelemetry/api-metrics and
+// @opentelemetry/core too.
+
+/**
+ * - globalThis (New standard)
+ * - self (Will return the current window instance for supported browsers)
+ * - window (fallback for older browser implementations)
+ * - global (NodeJS implementation)
+ * - <object> (When all else fails)
+ */
+
 /** only globals that common to node and browsers are allowed */
 // eslint-disable-next-line node/no-unsupported-features/es-builtins, no-undef
-export const _globalThis = typeof globalThis === 'object' ? globalThis : window;
+export const _globalThis: typeof globalThis =
+  typeof globalThis === 'object' ? globalThis :
+  typeof self === 'object' ? self :
+  typeof window === 'object' ? window :
+  typeof global === 'object' ? global :
+  {} as typeof globalThis;
