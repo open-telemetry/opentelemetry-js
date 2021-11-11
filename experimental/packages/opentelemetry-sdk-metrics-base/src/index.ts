7d67bf3e3e99908d74ffd99333805697334f0600
Daniel Dyla <dyladan@users.noreply.github.com>
2021-11-11 11:56:42 -0500

    chore: Remove old metrics SDK (#2598)

--- experimental/packages/opentelemetry-sdk-metrics-base/src/index.ts
+++ experimental/packages/opentelemetry-sdk-metrics-base/src/index.ts
@@ -14,16 +14,4 @@
  * limitations under the License.
  */
 
-export * from './BoundInstrument';
-export * from './CounterMetric';
-export * from './HistogramMetric';
-export * from './Meter';
-export * from './MeterProvider';
-export * from './Metric';
-export * from './ObservableGaugeMetric';
-export * from './export/aggregators';
-export * from './export/ConsoleMetricExporter';
-export * from './export/Processor';
-export * from './export/types';
-export * from './UpDownCounterMetric';
-export { MeterConfig } from './types';
+export * from './version';
