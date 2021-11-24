433374a33d53d7cafd8b569d5d95e3f48668e21a
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2021-11-24 21:32:56 +0530

    chore: indent

--- experimental/packages/opentelemetry-sdk-metrics-base/test/ExemplarFilter.test.ts
+++ experimental/packages/opentelemetry-sdk-metrics-base/test/ExemplarFilter.test.ts
@@ -28,41 +28,41 @@ describe('ExemplarFilter', () => {
   const TRACE_ID = 'd4cda95b652f4a1592b449d5929fda1b';
   const SPAN_ID = '6e0c63257de34c92';
 
-	describe('AlwaysSampleExemplarFilter', () => {
-		it('should return true always for shouldSample', () => {
-			const filter = new AlwaysSampleExemplarFilter();
-			assert.strictEqual(filter.shouldSample(10, [0, 0], {}, ROOT_CONTEXT), true);
-		});
-	})
+  describe('AlwaysSampleExemplarFilter', () => {
+    it('should return true always for shouldSample', () => {
+      const filter = new AlwaysSampleExemplarFilter();
+      assert.strictEqual(filter.shouldSample(10, [0, 0], {}, ROOT_CONTEXT), true);
+    });
+  });
 
-	describe('NeverSampleExemplarFilter', () => {
-		it('should return false always for shouldSample', () => {
-			const filter = new NeverSampleExemplarFilter()
-			assert.strictEqual(filter.shouldSample(1, [0, 0], {}, ROOT_CONTEXT), false);
-		});
-	});
+  describe('NeverSampleExemplarFilter', () => {
+    it('should return false always for shouldSample', () => {
+      const filter = new NeverSampleExemplarFilter()
+      assert.strictEqual(filter.shouldSample(1, [0, 0], {}, ROOT_CONTEXT), false);
+    });
+  });
 
-	describe('WithTraceExemplarFilter', () => {
-		it('should return false for shouldSample when the trace is not sampled', () => {
-			const filter = new WithTraceExemplarFilter();
+  describe('WithTraceExemplarFilter', () => {
+    it('should return false for shouldSample when the trace is not sampled', () => {
+      const filter = new WithTraceExemplarFilter();
       const spanContext: SpanContext = {
         traceId: TRACE_ID,
         spanId: SPAN_ID,
         traceFlags: TraceFlags.NONE,
       };
-			const ctx = trace.setSpanContext(ROOT_CONTEXT, spanContext)
-			assert.strictEqual(filter.shouldSample(5.3, [0, 0,], {}, ctx), false);
-		});
+      const ctx = trace.setSpanContext(ROOT_CONTEXT, spanContext)
+      assert.strictEqual(filter.shouldSample(5.3, [0, 0,], {}, ctx), false);
+    });
 
-		it('should return true for shouldSample when the trace is sampled', () => {
-			const filter = new WithTraceExemplarFilter();
+    it('should return true for shouldSample when the trace is sampled', () => {
+      const filter = new WithTraceExemplarFilter();
       const spanContext: SpanContext = {
         traceId: TRACE_ID,
         spanId: SPAN_ID,
         traceFlags: TraceFlags.SAMPLED,
       };
-			const ctx = trace.setSpanContext(ROOT_CONTEXT, spanContext)
-			assert.strictEqual(filter.shouldSample(5.3, [0, 0,], {}, ctx), true);
-		});
-	});
+      const ctx = trace.setSpanContext(ROOT_CONTEXT, spanContext)
+      assert.strictEqual(filter.shouldSample(5.3, [0, 0,], {}, ctx), true);
+    });
+  });
 });
