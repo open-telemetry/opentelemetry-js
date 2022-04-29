25b273fde203c206f71cf20b2fffb6b33ad41a91
legendecas <legendecas@gmail.com>
2022-04-29 11:06:59 +0800

    chore: update prometheus examples

--- experimental/examples/prometheus/index.js
+++ experimental/examples/prometheus/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
+const { DiagConsoleLogger, DiagLogLevel, diag } = require('@opentelemetry/api');
 const { MeterProvider } = require('@opentelemetry/sdk-metrics-base');
 const { PrometheusExporter } = require('@opentelemetry/exporter-prometheus');
+
+// Optional and only needed to see the internal diagnostic logging (during development)
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 const { endpoint, port } = PrometheusExporter.DEFAULT_OPTIONS;
 
@@ -11,11 +15,12 @@ const exporter = new PrometheusExporter({}, () => {
   );
 });
 
-const meter = new MeterProvider({
-  exporter,
-  interval: 1000,
-}).getMeter('example-prometheus');
+// Creates MeterProvider and installs the exporter as a MetricReader
+const meterProvider = new MeterProvider();
+meterProvider.addMetricReader(exporter);
+const meter = meterProvider.getMeter('example-prometheus');
 
+// Creates metric instruments
 const requestCounter = meter.createCounter('requests', {
   description: 'Example of a Counter',
 });
@@ -26,6 +31,7 @@ const upDownCounter = meter.createUpDownCounter('test_up_down_counter', {
 
 const labels = { pid: process.pid, environment: 'staging' };
 
+// Record metrics
 setInterval(() => {
   requestCounter.add(1, labels);
   upDownCounter.add(Math.random() > 0.5 ? 1 : -1, labels);
