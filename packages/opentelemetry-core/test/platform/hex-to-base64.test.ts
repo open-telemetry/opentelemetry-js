13471d75cd19829b62d15a41faf802410c105b9f
Siim Kallas <siimkallas@gmail.com>
2022-08-22 17:02:44 +0300

    test: add a test for longer hex strings

--- packages/opentelemetry-core/test/platform/hex-to-base64.test.ts
+++ packages/opentelemetry-core/test/platform/hex-to-base64.test.ts
@@ -25,5 +25,7 @@ describe('hexToBase64', () => {
     assert.strictEqual(hexToBase64(id1), 'fetzngLkTvI=');
     assert.strictEqual(hexToBase64(id2), 'EqvANNVn6J/ybmCMjPQsgA==');
     assert.strictEqual(hexToBase64(id3), 'EqvANNVn6J/ybmCMjPQsgA==');
+    // Don't use the preallocated path
+    assert.strictEqual(hexToBase64(id2.repeat(2)), 'EqvANNVn6J/ybmCMjPQsgBKrwDTVZ+if8m5gjIz0LIA=');
   });
 });
