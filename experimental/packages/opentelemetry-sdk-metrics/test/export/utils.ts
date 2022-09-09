f0581e7dfbfeaf880beb6fd950e47d5b0b134c26
marc.pichler <marc.pichler@dynatrace.com>
2022-09-09 07:36:48 +0200

    feat(sdk-metrics): use loop instead of duplication in reader test utils.

--- experimental/packages/opentelemetry-sdk-metrics/test/export/utils.ts
+++ experimental/packages/opentelemetry-sdk-metrics/test/export/utils.ts
@@ -22,19 +22,26 @@ import {
 } from '../../src';
 import * as assert from 'assert';
 
+const instrumentTypes = [
+  InstrumentType.COUNTER,
+  InstrumentType.OBSERVABLE_COUNTER,
+  InstrumentType.UP_DOWN_COUNTER,
+  InstrumentType.OBSERVABLE_UP_DOWN_COUNTER,
+  InstrumentType.HISTOGRAM,
+  InstrumentType.OBSERVABLE_GAUGE
+];
 
 /**
  * Check if AggregationSelectors behave in the same way.
  * @param reader
- * @param expectedAggregationSelector
+ * @param expectedSelector
  */
-export function assertAggregationSelector(reader: MetricReader, expectedAggregationSelector: AggregationSelector) {
-  assert.strictEqual(reader.selectAggregation(InstrumentType.COUNTER), expectedAggregationSelector(InstrumentType.COUNTER));
-  assert.strictEqual(reader.selectAggregation(InstrumentType.OBSERVABLE_COUNTER), expectedAggregationSelector(InstrumentType.OBSERVABLE_COUNTER));
-  assert.strictEqual(reader.selectAggregation(InstrumentType.UP_DOWN_COUNTER), expectedAggregationSelector(InstrumentType.UP_DOWN_COUNTER));
-  assert.strictEqual(reader.selectAggregation(InstrumentType.OBSERVABLE_UP_DOWN_COUNTER), expectedAggregationSelector(InstrumentType.OBSERVABLE_UP_DOWN_COUNTER));
-  assert.strictEqual(reader.selectAggregation(InstrumentType.HISTOGRAM), expectedAggregationSelector(InstrumentType.HISTOGRAM));
-  assert.strictEqual(reader.selectAggregation(InstrumentType.OBSERVABLE_GAUGE), expectedAggregationSelector(InstrumentType.OBSERVABLE_GAUGE));
+export function assertAggregationSelector(reader: MetricReader, expectedSelector: AggregationSelector) {
+  for (const instrumentType of instrumentTypes) {
+    assert.strictEqual(reader.selectAggregation(instrumentType),
+      expectedSelector(instrumentType),
+      `incorrect aggregation selection for ${InstrumentType[instrumentType]}`);
+  }
 }
 
 /**
@@ -43,10 +50,9 @@ export function assertAggregationSelector(reader: MetricReader, expectedAggregat
  * @param expectedSelector
  */
 export function assertAggregationTemporalitySelector(reader: MetricReader, expectedSelector: AggregationTemporalitySelector) {
-  assert.strictEqual(reader.selectAggregationTemporality(InstrumentType.COUNTER), expectedSelector(InstrumentType.COUNTER));
-  assert.strictEqual(reader.selectAggregationTemporality(InstrumentType.OBSERVABLE_COUNTER), expectedSelector(InstrumentType.OBSERVABLE_COUNTER));
-  assert.strictEqual(reader.selectAggregationTemporality(InstrumentType.UP_DOWN_COUNTER), expectedSelector(InstrumentType.UP_DOWN_COUNTER));
-  assert.strictEqual(reader.selectAggregationTemporality(InstrumentType.OBSERVABLE_UP_DOWN_COUNTER), expectedSelector(InstrumentType.OBSERVABLE_UP_DOWN_COUNTER));
-  assert.strictEqual(reader.selectAggregationTemporality(InstrumentType.OBSERVABLE_GAUGE), expectedSelector(InstrumentType.OBSERVABLE_GAUGE));
-  assert.strictEqual(reader.selectAggregationTemporality(InstrumentType.HISTOGRAM), expectedSelector(InstrumentType.HISTOGRAM));
+  for (const instrumentType of instrumentTypes) {
+    assert.strictEqual(reader.selectAggregationTemporality(instrumentType),
+      expectedSelector(instrumentType),
+      `incorrect aggregation temporality selection for ${InstrumentType[instrumentType]}`);
+  }
 }
