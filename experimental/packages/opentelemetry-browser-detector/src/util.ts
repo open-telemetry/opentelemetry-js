714769f08cb2ff45202063ac7953202cbcd8286d
Abinet <adebele@cisco.com>
2022-10-04 09:42:44 -0700

    Add Browser Detector Module

---
+++ experimental/packages/opentelemetry-browser-detector/src/util.ts
@@ -0,0 +1,120 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Suite} from 'mocha';
+import * as assert from 'assert';
+import {SemanticResourceAttributes} from '@opentelemetry/semantic-conventions';
+import {BROWSER_ATTRIBUTES} from './types';
+import {Resource} from '@opentelemetry/resources';
+
+export function describeBrowser(title: string, fn: (this: Suite) => void) {
+  title = `Browser: ${title}`;
+  if (typeof process === 'object' && process.release?.name === 'node') {
+    return describe.skip(title, fn);
+  }
+  return describe(title, fn);
+}
+
+export const assertResource = (
+  resource: Resource,
+  validations: {
+    pid?: number;
+    name?: string;
+    command?: string;
+    commandLine?: string;
+    version?: string;
+    runtimeName?: string;
+    runtimeDescription?: string;
+    platform?: string,
+    brands?: string[],
+    mobile?: boolean,
+    user_agent?: string
+  }
+) => {
+  assert.strictEqual(
+    resource.attributes[SemanticResourceAttributes.PROCESS_PID],
+    validations.pid
+  );
+  if (validations.name) {
+    assert.strictEqual(
+      resource.attributes[SemanticResourceAttributes.PROCESS_EXECUTABLE_NAME],
+      validations.name
+    );
+  }
+  if (validations.command) {
+    assert.strictEqual(
+      resource.attributes[SemanticResourceAttributes.PROCESS_COMMAND],
+      validations.command
+    );
+  }
+  if (validations.commandLine) {
+    assert.strictEqual(
+      resource.attributes[SemanticResourceAttributes.PROCESS_COMMAND_LINE],
+      validations.commandLine
+    );
+  }
+  if (validations.version) {
+    assert.strictEqual(
+      resource.attributes[SemanticResourceAttributes.PROCESS_RUNTIME_VERSION],
+      validations.version
+    );
+  }
+  if (validations.runtimeName) {
+    assert.strictEqual(
+      resource.attributes[SemanticResourceAttributes.PROCESS_RUNTIME_NAME],
+      validations.runtimeName
+    );
+  }
+  if (validations.runtimeDescription) {
+    assert.strictEqual(
+      resource.attributes[SemanticResourceAttributes.PROCESS_RUNTIME_DESCRIPTION],
+      validations.runtimeDescription
+    );
+  }
+  if (validations.platform) {
+    assert.strictEqual(
+      resource.attributes[BROWSER_ATTRIBUTES.PLATFORM],
+      validations.platform
+    );
+  }
+  if (validations.brands) {
+    assert.ok(Array.isArray(resource.attributes[BROWSER_ATTRIBUTES.BRANDS]));
+    assert.strictEqual(
+      (resource.attributes[BROWSER_ATTRIBUTES.BRANDS] as Array<string>).join('.'),
+      validations.brands.join('.')
+    );
+  }
+  if (validations.mobile) {
+    assert.strictEqual(
+      (resource.attributes[BROWSER_ATTRIBUTES.MOBILE]),
+      validations.mobile
+    );
+  }
+  if (validations.user_agent) {
+    assert.strictEqual(
+      (resource.attributes[BROWSER_ATTRIBUTES.USER_AGENT]),
+      validations.user_agent
+    );
+  }
+};
+
+/**
+ * Test utility method to validate an empty resource
+ *
+ * @param resource the Resource to validate
+ */
+export const assertEmptyResource = (resource: Resource) => {
+  assert.strictEqual(Object.keys(resource.attributes).length, 0);
+};
---
+++ experimental/packages/opentelemetry-browser-detector/test/BrowserDetector.test.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as sinon from 'sinon';
+import { Resource } from '@opentelemetry/resources';
+import { browserDetector } from '../src/BrowserDetector';
+import { describeBrowser } from '../src/util';
+import {
+  assertResource,
+  assertEmptyResource
+} from '../src/util';
+
+describeBrowser('browserDetector()', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('should return browser information', async () => {
+    sinon.stub(globalThis, 'navigator').value({
+      userAgent: 'dddd',
+      userAgentData:{
+        platform:'platform',
+        brands:['brand1'],
+        mobile:false
+      }
+    });
+
+    const resource: Resource = await browserDetector.detect();
+    assertResource(resource, {
+      version: 'dddd',
+      runtimeDescription: 'Web Browser',
+      runtimeName: 'browser',
+      platform: 'platform',
+      brands: ['brand1'],
+      mobile: false,
+      user_agent: 'dddd'
+    });
+  });
+  it('should return empty resources if version is missing', async () => {
+    sinon.stub(globalThis, 'navigator').value({
+      userAgent: '',
+    });
+    const resource: Resource = await browserDetector.detect();
+    assertEmptyResource(resource);
+  });
+});
