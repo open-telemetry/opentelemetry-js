b1f6992ba4a49ebbafa9e4a7410df8095a3c1a57
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2022-10-13 15:31:58 -0700

    Add tracing suppresing for Metrics Export

--- experimental/packages/opentelemetry-sdk-metrics/src/export/PeriodicExportingMetricReader.ts
+++ experimental/packages/opentelemetry-sdk-metrics/src/export/PeriodicExportingMetricReader.ts
@@ -18,6 +18,7 @@ import * as api from '@opentelemetry/api';
 import {
   ExportResultCode,
   globalErrorHandler,
+  suppressTracing,
   unrefTimer
 } from '@opentelemetry/core';
 import { MetricReader } from './MetricReader';
@@ -86,17 +87,19 @@ export class PeriodicExportingMetricReader extends MetricReader {
     }
 
     return new Promise((resolve, reject) => {
-      this._exporter.export(resourceMetrics, result => {
-        if (result.code !== ExportResultCode.SUCCESS) {
-          reject(
-            result.error ??
-            new Error(
-              `PeriodicExportingMetricReader: metrics export failed (error ${result.error})`
-            )
-          );
-        } else {
-          resolve();
-        }
+      api.context.with(suppressTracing(api.context.active()), () => {
+        this._exporter.export(resourceMetrics, result => {
+          if (result.code !== ExportResultCode.SUCCESS) {
+            reject(
+              result.error ??
+              new Error(
+                `PeriodicExportingMetricReader: metrics export failed (error ${result.error})`
+              )
+            );
+          } else {
+            resolve();
+          }
+        });
       });
     });
   }
