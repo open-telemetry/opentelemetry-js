c898ea2d89aca1e6b6413b10cbac9e2ca5f01a80
Santosh Cheler <scheler@cisco.com>
2023-01-06 11:49:33 -0800

    Fixes based on testing opentelemetry-web/fetch-proto

--- experimental/packages/otlp-exporter-base/src/index.ts
+++ experimental/packages/otlp-exporter-base/src/index.ts
@@ -13,16 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export {
-  OTLPExporterNodeBase,
-  sendWithHttp,
-  createHttpAgent,
-  configureCompression,
-  OTLPExporterNodeConfigBase,
-  CompressionAlgorithm,
-  OTLPExporterBrowserBase,
-  sendWithXhr,
-} from './platform';
+export * from './platform';
 export { OTLPExporterBase } from './OTLPExporterBase';
 export {
   OTLPExporterError,
--- experimental/packages/otlp-proto-exporter-base/src/index.ts
+++ experimental/packages/otlp-proto-exporter-base/src/index.ts
@@ -13,11 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export {
-  OTLPProtoExporterNodeBase,
-  OTLPProtoExporterBrowserBase,
-  ServiceClientType,
-  ExportRequestType,
-  getExportRequestProto,
-  send,
-} from './platform';
+export * from './platform';
