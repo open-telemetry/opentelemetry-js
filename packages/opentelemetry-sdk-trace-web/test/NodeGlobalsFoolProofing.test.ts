acf15e487c59ca559ec46836c084404a8bb520ae
Ravindra Rathor <ravindra@dyte.io>
2023-02-16 20:32:59 +0530

    style(lint): fixed the lint issues using --fix

--- packages/opentelemetry-sdk-trace-web/test/NodeGlobalsFoolProofing.test.ts
+++ packages/opentelemetry-sdk-trace-web/test/NodeGlobalsFoolProofing.test.ts
@@ -15,70 +15,70 @@
  */
 
 import {
-    context,
-    propagation,
-    trace,
-    ProxyTracerProvider,
-  } from '@opentelemetry/api';
-  import { Resource } from '@opentelemetry/resources';
-  import { Tracer } from '@opentelemetry/sdk-trace-base';
-  import * as assert from 'assert';
-  import { StackContextManager, WebTracerProvider } from '../src';
-  
-  describe('Node Globals Foolproofing', () => {
-    const originalProcess =  globalThis?.process;
-    before(() => {
-        Object.assign(globalThis, { process: false });
-    })
-      
-    after(() => {
-        Object.assign(globalThis, { process: originalProcess });
-    })
+  context,
+  propagation,
+  trace,
+  ProxyTracerProvider,
+} from '@opentelemetry/api';
+import { Resource } from '@opentelemetry/resources';
+import { Tracer } from '@opentelemetry/sdk-trace-base';
+import * as assert from 'assert';
+import { StackContextManager, WebTracerProvider } from '../src';
 
-    beforeEach(() => {
-      context.disable();
-      trace.disable();
-      propagation.disable();
-    });
-  
-    it('Can get TraceProvider without node globals such as process', () => {
-      const propagator = propagation['_getGlobalPropagator']();
-      const tracerProvider = new WebTracerProvider();
-      tracerProvider.register({
-        propagator: null,
-      });
-  
-      assert.strictEqual(
-        propagation['_getGlobalPropagator'](),
-        propagator,
-        'propagator should not change'
-      );
-  
-      assert.ok(context['_getContextManager']() instanceof StackContextManager);
-      const apiTracerProvider = trace.getTracerProvider() as ProxyTracerProvider;
-      assert.ok(apiTracerProvider.getDelegate() === tracerProvider);
-    });
-
-    it('Can get TraceProvider with custom id generator and without node globals such as process', () => {
-
-        const getRandomString = (length: number) => {
-            const alphanumericsList =  'abcdefghijklmnopqrstuvwxyz0123456789'.split("");
-            alphanumericsList.sort( () => .5 - Math.random() );
-            
-            return alphanumericsList.slice(0, length).join("");
-        }
-        
-        const tracer = new WebTracerProvider({
-            resource: new Resource({
-                'service.name': 'web-core',
-            }),
-            idGenerator: {
-                generateTraceId: () => getRandomString(32),
-                generateSpanId: () => getRandomString(16),
-            },
-        }).getTracer('default');
-
-        assert.ok(tracer instanceof Tracer);
-      });
+describe('Node Globals Foolproofing', () => {
+  const originalProcess = globalThis?.process;
+  before(() => {
+    Object.assign(globalThis, { process: false });
   });
-  
+
+  after(() => {
+    Object.assign(globalThis, { process: originalProcess });
+  });
+
+  beforeEach(() => {
+    context.disable();
+    trace.disable();
+    propagation.disable();
+  });
+
+  it('Can get TraceProvider without node globals such as process', () => {
+    const propagator = propagation['_getGlobalPropagator']();
+    const tracerProvider = new WebTracerProvider();
+    tracerProvider.register({
+      propagator: null,
+    });
+
+    assert.strictEqual(
+      propagation['_getGlobalPropagator'](),
+      propagator,
+      'propagator should not change'
+    );
+
+    assert.ok(context['_getContextManager']() instanceof StackContextManager);
+    const apiTracerProvider = trace.getTracerProvider() as ProxyTracerProvider;
+    assert.ok(apiTracerProvider.getDelegate() === tracerProvider);
+  });
+
+  it('Can get TraceProvider with custom id generator and without node globals such as process', () => {
+    const getRandomString = (length: number) => {
+      const alphanumericsList = 'abcdefghijklmnopqrstuvwxyz0123456789'.split(
+        ''
+      );
+      alphanumericsList.sort(() => 0.5 - Math.random());
+
+      return alphanumericsList.slice(0, length).join('');
+    };
+
+    const tracer = new WebTracerProvider({
+      resource: new Resource({
+        'service.name': 'web-core',
+      }),
+      idGenerator: {
+        generateTraceId: () => getRandomString(32),
+        generateSpanId: () => getRandomString(16),
+      },
+    }).getTracer('default');
+
+    assert.ok(tracer instanceof Tracer);
+  });
+});
