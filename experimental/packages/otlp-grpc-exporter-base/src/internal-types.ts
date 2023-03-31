fbd2d280b0e3c0b9d4d12e80604ce3e1cd2fdf13
Marc Pichler <marc.pichler@dynatrace.com>
2023-03-31 09:35:44 +0200

    feat(otlp-exporter-grpc-base): use type import for protobufjs

--- experimental/packages/otlp-grpc-exporter-base/src/internal-types.ts
+++ experimental/packages/otlp-grpc-exporter-base/src/internal-types.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import type * as protobuf from 'protobufjs';
+
 export interface ExportType<T, R = T & { toJSON: () => unknown }> {
   encode(message: T, writer?: protobuf.Writer): protobuf.Writer;
   decode(reader: protobuf.Reader | Uint8Array, length?: number): R;
