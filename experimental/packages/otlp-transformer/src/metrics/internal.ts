85b309cb7b357dbb3cfe76535d1ab1968f59d7ce
粒粒橙 <i@llc.moe>
2023-04-18 17:46:24 +0800

    Revert "refactor(otlp-transformer): improve code coverage for metrics"
    This reverts commit fdd06c6ff97bc8c16c716bdaae8f86a52137940a.

--- experimental/packages/otlp-transformer/src/metrics/internal.ts
+++ experimental/packages/otlp-transformer/src/metrics/internal.ts
@@ -44,21 +44,24 @@ export function toResourceMetrics(
       attributes: toAttributes(resourceMetrics.resource.attributes),
       droppedAttributesCount: 0,
     },
-    schemaUrl: undefined,
+    schemaUrl: undefined, // TODO: Schema Url does not exist yet in the SDK.
     scopeMetrics: toScopeMetrics(resourceMetrics.scopeMetrics),
   };
 }
 
 export function toScopeMetrics(scopeMetrics: ScopeMetrics[]): IScopeMetrics[] {
   return Array.from(
-    scopeMetrics.map(metrics => ({
-      scope: {
-        name: metrics.scope.name,
-        version: metrics.scope.version,
-      },
-      metrics: metrics.metrics.map(metricData => toMetric(metricData)),
-      schemaUrl: metrics.scope.schemaUrl,
-    }))
+    scopeMetrics.map(metrics => {
+      const scopeMetrics: IScopeMetrics = {
+        scope: {
+          name: metrics.scope.name,
+          version: metrics.scope.version,
+        },
+        metrics: metrics.metrics.map(metricData => toMetric(metricData)),
+        schemaUrl: metrics.scope.schemaUrl,
+      };
+      return scopeMetrics;
+    })
   );
 }
 
@@ -73,31 +76,27 @@ export function toMetric(metricData: MetricData): IMetric {
     metricData.aggregationTemporality
   );
 
-  switch (metricData.dataPointType) {
-    case DataPointType.SUM:
-      out.sum = {
-        aggregationTemporality,
-        isMonotonic: metricData.isMonotonic,
-        dataPoints: toSingularDataPoints(metricData),
-      };
-      break;
-    case DataPointType.GAUGE:
-      out.gauge = {
-        dataPoints: toSingularDataPoints(metricData),
-      };
-      break;
-    case DataPointType.HISTOGRAM:
-      out.histogram = {
-        aggregationTemporality,
-        dataPoints: toHistogramDataPoints(metricData),
-      };
-      break;
-    case DataPointType.EXPONENTIAL_HISTOGRAM:
-      out.exponentialHistogram = {
-        aggregationTemporality,
-        dataPoints: toExponentialHistogramDataPoints(metricData),
-      };
-      break;
+  if (metricData.dataPointType === DataPointType.SUM) {
+    out.sum = {
+      aggregationTemporality,
+      isMonotonic: metricData.isMonotonic,
+      dataPoints: toSingularDataPoints(metricData),
+    };
+  } else if (metricData.dataPointType === DataPointType.GAUGE) {
+    // Instrument is a gauge.
+    out.gauge = {
+      dataPoints: toSingularDataPoints(metricData),
+    };
+  } else if (metricData.dataPointType === DataPointType.HISTOGRAM) {
+    out.histogram = {
+      aggregationTemporality,
+      dataPoints: toHistogramDataPoints(metricData),
+    };
+  } else if (metricData.dataPointType === DataPointType.EXPONENTIAL_HISTOGRAM) {
+    out.exponentialHistogram = {
+      aggregationTemporality,
+      dataPoints: toExponentialHistogramDataPoints(metricData),
+    };
   }
 
   return out;
@@ -116,13 +115,10 @@ function toSingularDataPoint(
     timeUnixNano: hrTimeToNanoseconds(dataPoint.endTime),
   };
 
-  switch (valueType) {
-    case ValueType.INT:
-      out.asInt = dataPoint.value as number;
-      break;
-    case ValueType.DOUBLE:
-      out.asDouble = dataPoint.value as number;
-      break;
+  if (valueType === ValueType.INT) {
+    out.asInt = dataPoint.value as number;
+  } else if (valueType === ValueType.DOUBLE) {
+    out.asDouble = dataPoint.value as number;
   }
 
   return out;
@@ -181,10 +177,13 @@ function toExponentialHistogramDataPoints(
 function toAggregationTemporality(
   temporality: AggregationTemporality
 ): EAggregationTemporality {
-  switch (temporality) {
-    case AggregationTemporality.DELTA:
-      return EAggregationTemporality.AGGREGATION_TEMPORALITY_DELTA;
-    case AggregationTemporality.CUMULATIVE:
-      return EAggregationTemporality.AGGREGATION_TEMPORALITY_CUMULATIVE;
+  if (temporality === AggregationTemporality.DELTA) {
+    return EAggregationTemporality.AGGREGATION_TEMPORALITY_DELTA;
   }
+
+  if (temporality === AggregationTemporality.CUMULATIVE) {
+    return EAggregationTemporality.AGGREGATION_TEMPORALITY_CUMULATIVE;
+  }
+
+  return EAggregationTemporality.AGGREGATION_TEMPORALITY_UNSPECIFIED;
 }
