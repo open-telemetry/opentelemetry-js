b2e09685216697bd78c6ef314dc90bfa06e1d95a
Abinet <adebele@cisco.com>
2023-05-16 12:16:32 -0700

    feat: Support map type in log attributes

--- experimental/packages/sdk-logs/src/LogRecord.ts
+++ experimental/packages/sdk-logs/src/LogRecord.ts
@@ -27,13 +27,14 @@ import type { IResource } from '@opentelemetry/resources';
 import type { ReadableLogRecord } from './export/ReadableLogRecord';
 import type { LogRecordLimits } from './types';
 import { Logger } from './Logger';
+import {LogAttributes} from "@opentelemetry/api-logs";
 
 export class LogRecord implements ReadableLogRecord {
   readonly hrTime: api.HrTime;
   readonly spanContext?: api.SpanContext;
   readonly resource: IResource;
   readonly instrumentationScope: InstrumentationScope;
-  readonly attributes: Attributes = {};
+  readonly attributes: logsAPI.LogAttributes = {};
   private _severityText?: string;
   private _severityNumber?: logsAPI.SeverityNumber;
   private _body?: string;
@@ -97,12 +98,19 @@ export class LogRecord implements ReadableLogRecord {
     this.setAttributes(attributes);
   }
 
-  public setAttribute(key: string, value?: AttributeValue) {
+  public setAttribute(key: string, value?: Attributes | AttributeValue) {
     if (this._isLogRecordReadonly()) {
       return this;
     }
     if (value === null) {
       return this;
+    }
+    if (
+      typeof value === 'object' &&
+      !Array.isArray(value) &&
+      Object.keys(value).length > 0
+    ) {
+      this.attributes[key] = value;
     }
     if (key.length === 0) {
       api.diag.warn(`Invalid attribute key: ${key}`);
@@ -123,7 +131,7 @@ export class LogRecord implements ReadableLogRecord {
     return this;
   }
 
-  public setAttributes(attributes: Attributes) {
+  public setAttributes(attributes: LogAttributes) {
     for (const [k, v] of Object.entries(attributes)) {
       this.setAttribute(k, v);
     }
