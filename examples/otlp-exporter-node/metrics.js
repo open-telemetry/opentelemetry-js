be8014b6d8be140fc376ba5b04ac02e2c9b7e9ab
JamieDanielson <jamieedanielson@gmail.com>
2023-06-02 14:49:43 -0400

    docs: add example of exponential histogram

--- examples/otlp-exporter-node/metrics.js
+++ examples/otlp-exporter-node/metrics.js
@@ -4,25 +4,50 @@ const { DiagConsoleLogger, DiagLogLevel, diag } = require('@opentelemetry/api');
 const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-http');
 // const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-grpc');
 // const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
-const { MeterProvider, PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
+// const { ConsoleMetricExporter } = require('@opentelemetry/sdk-metrics');
+const {
+  ExponentialHistogramAggregation,
+  MeterProvider,
+  PeriodicExportingMetricReader,
+  View,
+} = require('@opentelemetry/sdk-metrics');
 const { Resource } = require('@opentelemetry/resources');
-const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+const {
+  SemanticResourceAttributes,
+} = require('@opentelemetry/semantic-conventions');
 
 // Optional and only needed to see the internal diagnostic logging (during development)
 diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
-const metricExporter = new OTLPMetricExporter({});
+const metricExporter = new OTLPMetricExporter({
+  // headers: {
+  //   foo: 'bar'
+  // },
+});
 
+// Define view for the exponential histogram metric
+const expHistogramView = new View({
+  aggregation: new ExponentialHistogramAggregation(),
+  instrumentName: 'test_exponential_histogram',
+});
+// Note, the instrumentName is the same as the name that has been passed for
+// the Meter#createHistogram function for exponentialHistogram.
+
+// Create an instance of the metric provider
 const meterProvider = new MeterProvider({
   resource: new Resource({
     [SemanticResourceAttributes.SERVICE_NAME]: 'basic-metric-service',
   }),
+  views: [expHistogramView],
 });
 
-meterProvider.addMetricReader(new PeriodicExportingMetricReader({
-  exporter: metricExporter,
-  exportIntervalMillis: 1000,
-}));
+meterProvider.addMetricReader(
+  new PeriodicExportingMetricReader({
+    exporter: metricExporter,
+    // exporter: new ConsoleMetricExporter(),
+    exportIntervalMillis: 1000,
+  })
+);
 
 const meter = meterProvider.getMeter('example-exporter-collector');
 
@@ -36,6 +61,11 @@ const upDownCounter = meter.createUpDownCounter('test_up_down_counter', {
 
 const histogram = meter.createHistogram('test_histogram', {
   description: 'Example of a Histogram',
+  boundaries: [0, 10, 100, 1000],
+});
+
+const exponentialHistogram = meter.createHistogram('test_exp_histogram', {
+  description: 'Example of an ExponentialHistogram',
 });
 
 const attributes = { pid: process.pid, environment: 'staging' };
@@ -44,4 +74,5 @@ setInterval(() => {
   requestCounter.add(1, attributes);
   upDownCounter.add(Math.random() > 0.5 ? 1 : -1, attributes);
   histogram.record(Math.random(), attributes);
+  exponentialHistogram.record(Math.random(), attributes);
 }, 1000);
