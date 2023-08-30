2f6d80872254af585799fa49a7d06c7f4d9a90ab
Siim Kallas <siimkallas@gmail.com>
2023-08-30 22:01:36 +0300

    chore: update copyright for long.js
    Co-authored-by: Marc Pichler <marc.pichler@dynatrace.com>

--- experimental/packages/otlp-transformer/src/common/unsigned_long.ts
+++ experimental/packages/otlp-transformer/src/common/unsigned_long.ts
@@ -1,4 +1,6 @@
 /*
+ * Copyright 2009 The Closure Library Authors
+ * Copyright 2020 Daniel Wirtz / The long.js Authors.
  * Copyright The OpenTelemetry Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
