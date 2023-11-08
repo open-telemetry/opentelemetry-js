e6a11c3b17787af91ce22c88682ff3d9972ad1d1
github-action-benchmark <github@users.noreply.github.com>
2023-11-08 18:25:13 +0000

    add Benchmark (benchmarkjs) benchmark result for f665499096189390e691cf1a772e677fa67812d7

---
+++ benchmarks/data.js
@@ -0,0 +1,39 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1699467913610,
+  "repoUrl": "https://github.com/open-telemetry/opentelemetry-js",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "marc.pichler@dynatrace.com",
+            "name": "Marc Pichler",
+            "username": "pichlermarc"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "f665499096189390e691cf1a772e677fa67812d7",
+          "message": "chore: prepare release 1.18.1/0.45.1 (#4261)",
+          "timestamp": "2023-11-08T18:51:43+01:00",
+          "tree_id": "219e74d0332c27a3f0824c70d4863a5b74739f74",
+          "url": "https://github.com/open-telemetry/opentelemetry-js/commit/f665499096189390e691cf1a772e677fa67812d7"
+        },
+        "date": 1699467911124,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "create spans (10 attributes)",
+            "value": 724329,
+            "range": "±1.36%",
+            "unit": "ops/sec",
+            "extra": "92 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
