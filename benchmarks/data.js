8675f6de95b097fc5cd73885bf1bcb9537901a16
github-action-benchmark <github@users.noreply.github.com>
2023-11-09 13:33:19 +0000

    add Benchmark (benchmarkjs) benchmark result for c478c11975a3ab124822cb1dc9033a2adb21f44c

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699467913610,
+  "lastUpdate": 1699536799163,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-js",
   "entries": {
     "Benchmark": [
@@ -31,6 +31,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.36%",
             "unit": "ops/sec",
             "extra": "92 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "trentm@gmail.com",
+            "name": "Trent Mick",
+            "username": "trentm"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "c478c11975a3ab124822cb1dc9033a2adb21f44c",
+          "message": "chore: no need for 'packages' in \"lerna.json\" (#4264)",
+          "timestamp": "2023-11-09T14:32:18+01:00",
+          "tree_id": "680d1c638e619a0c89d0231db8191e7a507fa683",
+          "url": "https://github.com/open-telemetry/opentelemetry-js/commit/c478c11975a3ab124822cb1dc9033a2adb21f44c"
+        },
+        "date": 1699536798290,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "create spans (10 attributes)",
+            "value": 726370,
+            "range": "±0.19%",
+            "unit": "ops/sec",
+            "extra": "96 samples"
           }
         ]
       }
