0775e5955e8497b3834451b380df5b01f0bebf31
Marc Pichler <marc.pichler@dynatrace.com>
2024-02-01 14:27:29 +0100

    feat(otlp-proto-exporter-base): export protobuf types

--- experimental/packages/otlp-proto-exporter-base/src/index.ts
+++ experimental/packages/otlp-proto-exporter-base/src/index.ts
@@ -13,4 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import * as root from './generated/root';
+
+export const ExportMetricsServiceRequest =
+  root.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceRequest;
+export const ExportMetricsServiceResponse =
+  root.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse;
+
+export const ExportLogsServiceRequest =
+  root.opentelemetry.proto.collector.logs.v1.ExportLogsServiceRequest;
+export const ExportLogsServiceResponse =
+  root.opentelemetry.proto.collector.logs.v1.ExportLogsServiceResponse;
+
+export const ExportTraceServiceRequest =
+  root.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
+export const ExportTraceServiceResponse =
+  root.opentelemetry.proto.collector.metrics.v1.ExportMetricsServiceResponse;
+
+export type IExportMetricsServiceRequest =
+  root.opentelemetry.proto.collector.metrics.v1.IExportMetricsServiceRequest;
+export type IExportLogsServiceRequest =
+  root.opentelemetry.proto.collector.logs.v1.IExportLogsServiceRequest;
+export type IExportTraceServiceRequest =
+  root.opentelemetry.proto.collector.trace.v1.IExportTraceServiceRequest;
+
 export * from './legacy/platform';
