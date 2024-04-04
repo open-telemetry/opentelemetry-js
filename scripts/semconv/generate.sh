5bf2029314d41e82026aa35871b945ab1943c60e
Maikel <maikel.van.dort@gmail.com>
2024-04-04 21:38:00 +0200

    fix: remove double comment prefix

--- scripts/semconv/generate.sh
+++ scripts/semconv/generate.sh
@@ -67,6 +67,6 @@ cd "$ROOT_DIR"
 
 npm run lint:fix:changed
 
-# # Run the size checks for the generated files
+# Run the size checks for the generated files
 cd "${ROOT_DIR}/packages/opentelemetry-semantic-conventions"
 npm run size-check
