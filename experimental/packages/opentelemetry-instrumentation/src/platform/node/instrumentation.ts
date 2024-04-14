34a3a447d86e42fc89dda3bd55c8edc246eda746
Amir Blum <amirgiraffe@gmail.com>
2024-04-14 10:29:52 +0300

    chore: lint

--- experimental/packages/opentelemetry-instrumentation/src/platform/node/instrumentation.ts
+++ experimental/packages/opentelemetry-instrumentation/src/platform/node/instrumentation.ts
@@ -244,20 +244,10 @@ export abstract class InstrumentationBase
     this._warnOnPreloadedModules();
     for (const module of this._modules) {
       const hookFn: HookFn = (exports, name, baseDir) => {
-        return this._onRequire<typeof exports>(
-          module,
-          exports,
-          name,
-          baseDir
-        );
+        return this._onRequire<typeof exports>(module, exports, name, baseDir);
       };
       const onRequire: OnRequireFn = (exports, name, baseDir) => {
-        return this._onRequire<typeof exports>(
-          module,
-          exports,
-          name,
-          baseDir
-        );
+        return this._onRequire<typeof exports>(module, exports, name, baseDir);
       };
 
       // `RequireInTheMiddleSingleton` does not support absolute paths.
