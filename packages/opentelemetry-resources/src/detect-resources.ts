7f4ed0859ecd7922f51aac50238176291cbda1ba
Jakub <jakub@rho.co>
2024-05-09 11:16:54 +0200

    fix(resources): lint wait for async attributes for detecting resources

--- packages/opentelemetry-resources/src/detect-resources.ts
+++ packages/opentelemetry-resources/src/detect-resources.ts
@@ -70,7 +70,7 @@ export const detectResourcesSync = (
         if (isPromiseLike<Resource>(resourceOrPromise)) {
           const createPromise = async () => {
             const resolvedResource = await resourceOrPromise;
-            // await resolvedResource.waitForAsyncAttributes?.();
+            await resolvedResource.waitForAsyncAttributes?.();
             return resolvedResource.attributes;
           };
           resource = new Resource({}, createPromise());
