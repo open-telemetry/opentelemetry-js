da017e981ef61b29d004ad13974292196e4519f4
Amir Blum <amirgiraffe@gmail.com>
2024-05-10 09:55:21 +0300

    docs: lint fix

--- experimental/packages/opentelemetry-instrumentation/src/types.ts
+++ experimental/packages/opentelemetry-instrumentation/src/types.ts
@@ -97,16 +97,16 @@ export interface InstrumentationModuleFile {
   moduleExports?: unknown;
 
   /** Supported versions for the file.
-   * 
+   *
    * A module version is supported if one of the supportedVersions in the array satisfies the module version.
-   * The syntax ot the version is checked with the `satisfies` function of "The semantic versioner for npm", see 
+   * The syntax ot the version is checked with the `satisfies` function of "The semantic versioner for npm", see
    * [`semver` package](https://www.npmjs.com/package/semver)
    * If the version is not supported, we won't apply instrumentation patch.
    * If omitted, all versions of the module will be patched.
-   * 
+   *
    * It is recommended to always specify a range that is bound to a major version, to avoid breaking changes.
    * New major versions should be reviewed and tested before being added to the supportedVersions array.
-   * 
+   *
    * Example: ['>=1.2.3 <3']
    */
   supportedVersions: string[];
@@ -131,16 +131,16 @@ export interface InstrumentationModuleDefinition {
   moduleVersion?: string;
 
   /** Supported version of module.
-   * 
+   *
    * A module version is supported if one of the supportedVersions in the array satisfies the module version.
-   * The syntax ot the version is checked with the `satisfies` function of "The semantic versioner for npm", see 
+   * The syntax ot the version is checked with the `satisfies` function of "The semantic versioner for npm", see
    * [`semver` package](https://www.npmjs.com/package/semver)
    * If the version is not supported, we won't apply instrumentation patch (see `enable` method).
    * If omitted, all versions of the module will be patched.
-   * 
+   *
    * It is recommended to always specify a range that is bound to a major version, to avoid breaking changes.
    * New major versions should be reviewed and tested before being added to the supportedVersions array.
-   * 
+   *
    * Example: ['>=1.2.3 <3']
    */
   supportedVersions: string[];
