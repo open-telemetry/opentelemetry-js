f0764cc8e7d896c75ce2e43ed488f72ac9f19532
Zen-cronic <83657429+Zen-cronic@users.noreply.github.com>
2024-06-03 14:03:55 -0400

    refactor(examples/http): actually use new exported string constants for semconv

--- examples/http/tracer.js
+++ examples/http/tracer.js
@@ -15,7 +15,7 @@ const EXPORTER = process.env.EXPORTER || '';
 module.exports = (serviceName) => {
   const provider = new NodeTracerProvider({
     resource: new Resource({
-      [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
+      [SEMRESATTRS_SERVICE_NAME]: serviceName,
     }),
   });
 
