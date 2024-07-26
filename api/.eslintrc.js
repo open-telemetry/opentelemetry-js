e1aab56e518b7101cdafa7a761797ee30de136d0
Robb Kidd <robbkidd@honeycomb.io>
2024-07-26 11:27:36 -0400

    declare the rule at the root
    Remove special case eslint rule from API.

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -4,9 +4,5 @@ module.exports = {
         "commonjs": true,
         "shared-node-browser": true
     },
-    ...require('../eslint.base.js'),
-    "rules": { // needs to come after requiring eslint.base or the rule isn't picked up
-        // move to root eslint.base when all packages pass this rule
-        "no-restricted-syntax": ["error", "ExportAllDeclaration"],
-    },
+    ...require('../eslint.base.js')
 }
