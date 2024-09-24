696107b89006c23541e6735aa681628fc45b49d4
Marc Pichler <marc.pichler@dynatrace.com>
2024-09-24 15:07:50 +0200

    fixup! fix(sdk-logs): align LogRecord with new types

--- experimental/packages/sdk-logs/src/LogRecord.ts
+++ experimental/packages/sdk-logs/src/LogRecord.ts
@@ -26,7 +26,7 @@ import type { IResource } from '@opentelemetry/resources';
 
 import type { ReadableLogRecord } from './export/ReadableLogRecord';
 import type { LogRecordLimits } from './types';
-import {AnyValue, LogAttributes, LogBody} from '@opentelemetry/api-logs';
+import { AnyValue, LogAttributes, LogBody } from '@opentelemetry/api-logs';
 import { LoggerProviderSharedState } from './internal/LoggerProviderSharedState';
 
 export class LogRecord implements ReadableLogRecord {
