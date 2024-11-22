a241bf6195eb7c795ae7fa963b5aa94f7a579fb7
Marc Pichler <marc.pichler@dynatrace.com>
2024-11-22 11:45:54 +0100

    fix(otlp-exporter-base): don't create blob before sending xhr

--- experimental/packages/otlp-exporter-base/test/browser/xhr-transport.test.ts
+++ experimental/packages/otlp-exporter-base/test/browser/xhr-transport.test.ts
@@ -65,10 +65,6 @@ describe('XhrTransport', function () {
             undefined
           );
           assert.strictEqual(request.url, testTransportParameters.url);
-          assert.strictEqual(
-            (request.requestBody as unknown as Blob).type,
-            'application/json'
-          );
           ensureHeadersContain(request.requestHeaders, {
             foo: 'foo-value',
             bar: 'bar-value',
