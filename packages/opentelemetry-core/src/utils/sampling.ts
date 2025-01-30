51a607bfd87be27460c789647558e411f71faeca
Marc Pichler <marc.pichler@dynatrace.com>
2025-01-30 17:25:00 +0100

    feat(core)!: remove TracesSamplerValues from exports

--- packages/opentelemetry-core/src/utils/sampling.ts
+++ packages/opentelemetry-core/src/utils/sampling.ts
@@ -14,11 +14,4 @@
  * limitations under the License.
  */
 
-export enum TracesSamplerValues {
-  AlwaysOff = 'always_off',
-  AlwaysOn = 'always_on',
-  ParentBasedAlwaysOff = 'parentbased_always_off',
-  ParentBasedAlwaysOn = 'parentbased_always_on',
-  ParentBasedTraceIdRatio = 'parentbased_traceidratio',
-  TraceIdRatio = 'traceidratio',
-}
+
