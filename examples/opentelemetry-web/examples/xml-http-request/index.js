bdf32873cbff4919af00de3f35c7b5a65987eecf
Marc Pichler <marc.pichler@dynatrace.com>
2025-02-19 12:11:08 +0100

    docs(resources): align documentation with current state (#5486)

--- examples/opentelemetry-web/examples/xml-http-request/index.js
+++ examples/opentelemetry-web/examples/xml-http-request/index.js
@@ -6,11 +6,11 @@ const { ZoneContextManager } = require( '@opentelemetry/context-zone');
 const { OTLPTraceExporter } = require( '@opentelemetry/exporter-trace-otlp-http');
 const { B3Propagator } = require( '@opentelemetry/propagator-b3');
 const { registerInstrumentations } = require( '@opentelemetry/instrumentation');
-const { Resource } = require('@opentelemetry/resources');
+const { resourceFromAttributes } = require('@opentelemetry/resources');
 const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions');
 
 const providerWithZone = new WebTracerProvider({
-  resource: new Resource({
+  resource: resourceFromAttributes({
     [SEMRESATTRS_SERVICE_NAME]: 'xml-http-web-service'
   }),
   // Note: For production consider using the "BatchSpanProcessor" to reduce the number of requests
