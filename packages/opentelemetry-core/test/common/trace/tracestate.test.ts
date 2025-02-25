3b3d322d3dba3c453f1b9cb0eb0d388a7f77602a
David Luna <david.luna@elastic.co>
2025-02-25 13:40:36 +0100

    Update packages/opentelemetry-core/test/common/trace/tracestate.test.ts
    Co-authored-by: Jamie Danielson <jamiedanielson@honeycomb.io>

--- packages/opentelemetry-core/test/common/trace/tracestate.test.ts
+++ packages/opentelemetry-core/test/common/trace/tracestate.test.ts
@@ -115,7 +115,7 @@ describe('TraceState', () => {
           .map((_: null, num: number) => `a${num}=${num}`)
           .join(',')
       );
-      // assert.deepStrictEqual(state['_keys']().length, 32);
+      assert.deepStrictEqual(state.serialize().split(',').length, 32);
       assert.deepStrictEqual(state.get('a0'), '0');
       assert.deepStrictEqual(state.get('a31'), '31');
       assert.deepStrictEqual(
