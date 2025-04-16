872cbe9e91a0024290e091f340768763201652e1
Trent Mick <trentm@gmail.com>
2025-04-16 08:51:27 -0700

    move ATTR_PROCESS_RUNTIME_NAME unstable import to a local src/semconv.ts

--- packages/opentelemetry-core/src/platform/node/sdk-info.ts
+++ packages/opentelemetry-core/src/platform/node/sdk-info.ts
@@ -17,11 +17,11 @@
 import { VERSION } from '../../version';
 import {
   ATTR_TELEMETRY_SDK_NAME,
-  ATTR_PROCESS_RUNTIME_NAME,
   ATTR_TELEMETRY_SDK_LANGUAGE,
   TELEMETRY_SDK_LANGUAGE_VALUE_NODEJS,
   ATTR_TELEMETRY_SDK_VERSION,
 } from '@opentelemetry/semantic-conventions';
+import { ATTR_PROCESS_RUNTIME_NAME } from '../../semconv';
 
 /** Constants describing the SDK in use */
 export const SDK_INFO = {
