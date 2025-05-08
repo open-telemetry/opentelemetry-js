eb4ce206b764f0347bdec3b2d29330606ed5f8c9
Trent Mick <trentm@gmail.com>
2025-05-08 06:42:30 -0700

    feat(instrumentation): New utilities for semconv stability migration for instrumentations that produce 'http' and 'db' telemetry. (#5659)
    Co-authored-by: Jamie Danielson <jamiedanielson@honeycomb.io>

---
+++ experimental/packages/opentelemetry-instrumentation/test/common/semconvStability.test.ts
@@ -0,0 +1,59 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { inspect } from 'util';
+import * as assert from 'assert';
+import {
+  SemconvStability,
+  httpSemconvStabilityFromStr,
+  databaseSemconvStabilityFromStr,
+} from '../../src';
+
+describe('httpSemconvStabilityFromStr', function () {
+  const table = [
+    { str: undefined, expected: SemconvStability.OLD },
+    { str: '', expected: SemconvStability.OLD },
+    { str: 'http', expected: SemconvStability.STABLE },
+    { str: 'http/dup', expected: SemconvStability.DUPLICATE },
+    { str: ', http/dup,bar', expected: SemconvStability.DUPLICATE },
+    { str: ', http/dup\t ,blah', expected: SemconvStability.DUPLICATE },
+    { str: 'database', expected: SemconvStability.OLD },
+    { str: 'just,bogus,values', expected: SemconvStability.OLD },
+  ];
+  for (const { str, expected } of table) {
+    it(`str: ${inspect(str)}`, function () {
+      assert.strictEqual(httpSemconvStabilityFromStr(str), expected);
+    });
+  }
+});
+
+describe('databaseSemconvStabilityFromStr', function () {
+  const table = [
+    { str: undefined, expected: SemconvStability.OLD },
+    { str: '', expected: SemconvStability.OLD },
+    { str: 'database', expected: SemconvStability.STABLE },
+    { str: 'database/dup', expected: SemconvStability.DUPLICATE },
+    { str: ', database/dup,bar', expected: SemconvStability.DUPLICATE },
+    { str: ', database/dup\t ,blah', expected: SemconvStability.DUPLICATE },
+    { str: 'http', expected: SemconvStability.OLD },
+    { str: 'just,bogus,values', expected: SemconvStability.OLD },
+  ];
+  for (const { str, expected } of table) {
+    it(`str: ${inspect(str)}`, function () {
+      assert.strictEqual(databaseSemconvStabilityFromStr(str), expected);
+    });
+  }
+});
