27c3346b610b0bd5f70469f75218b2203193aaf1
Daniel Dyla <dyladan@users.noreply.github.com>
2025-05-23 15:21:32 -0400

    Verify metric value

--- e2e/verify.sh
+++ e2e/verify.sh
@@ -12,6 +12,12 @@ if [ "$METRIC_NAME" != "example_counter" ]; then
     exit 1
 fi
 
+METRIC_VALUE=$(jq -r 'select(has("resourceMetrics")) | .resourceMetrics[0].scopeMetrics[0].metrics[0].sum.dataPoints[0].value' otel-collector-output.json)
+if [ "$METRIC_VALUE" != "1" ]; then
+    echo "Expected metric value '1', but got '$METRIC_VALUE'"
+    exit 1
+fi
+
 LOG_BODY=$(jq -r 'select(has("resourceLogs")) | .resourceLogs[0].scopeLogs[0].logRecords[0].body.stringValue' otel-collector-output.json)
 if [ "$LOG_BODY" != "test-log-body" ]; then
     echo "Expected log body 'test-log-body', but got '$LOG_BODY'"
