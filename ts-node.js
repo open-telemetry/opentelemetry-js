ca15d419e8048762b8e8716b64932236a66e220b
Raphaël Thériault <raphael.theriault@solarwinds.com>
2025-06-10 15:09:14 -0400

    fix: make mocha esm patching opt-in

--- ts-node.js
+++ ts-node.js
@@ -1,8 +1,8 @@
-const { register } = require('module');
-const { pathToFileURL } = require('url');
-
 require('ts-node/register');
 
-if (process.env.MOCHA_DONT_PATCH_ESM == null) {
+if (process.env.MOCHA_PATCH_ESM != null) {
+  const { register } = require('module');
+  const { pathToFileURL } = require('url');
+
   register('ts-node/esm', pathToFileURL(__filename));
 }
