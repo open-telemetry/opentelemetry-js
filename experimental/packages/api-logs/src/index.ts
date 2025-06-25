cb0461a9391da16b5d09433aacf0037ea5eeae60
Alex Velikanov <alex25368@gmail.com>
2025-06-25 15:53:39 -0400

    fix(api-logs,sdk-logs): allow AnyValue attributes for logs and handle circular json

--- experimental/packages/api-logs/src/index.ts
+++ experimental/packages/api-logs/src/index.ts
@@ -20,6 +20,7 @@ export { SeverityNumber } from './types/LogRecord';
 export type { LogAttributes, LogBody, LogRecord } from './types/LogRecord';
 export type { LoggerOptions } from './types/LoggerOptions';
 export type { AnyValue, AnyValueMap } from './types/AnyValue';
+export { isLogAttributeValue } from './utils/validation';
 export { NOOP_LOGGER, NoopLogger } from './NoopLogger';
 export { NOOP_LOGGER_PROVIDER, NoopLoggerProvider } from './NoopLoggerProvider';
 export { ProxyLogger } from './ProxyLogger';
