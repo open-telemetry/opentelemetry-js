901e17a8720aaeecf1baaa11d3f45db717b36251
maryliag <marylia.gutierrez@grafana.com>
2025-07-24 15:41:30 -0400

    initial implementation of ConfigProvider

---
+++ experimental/packages/opentelemetry-configuration/src/ConfigProvider.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Configuration, initializeDefaultConfiguration } from "./config";
+import { diagLogLevelFromString } from "./utils/diagLogLevel";
+import { getBooleanFromEnv, getStringFromEnv, getStringListFromEnv } from "./utils/environment";
+
+/**
+ * ConfigProvider provides a configuration.
+ */
+export class ConfigProvider {
+    private _config: Configuration;
+
+    constructor() {
+        this._config = initializeDefaultConfiguration();
+        this._config.disable = getBooleanFromEnv('OTEL_SDK_DISABLED');
+
+        const logLevel = getStringFromEnv('OTEL_LOG_LEVEL');
+        if (logLevel) {
+            this._config.log_level = diagLogLevelFromString(logLevel);
+        }
+
+        const nodeResourceDetectors = getStringListFromEnv('OTEL_NODE_RESOURCE_DETECTORS');
+        if (nodeResourceDetectors) {
+            this._config.node_resource_detectors = nodeResourceDetectors;
+        }
+
+        const resourceAttrList = getStringFromEnv('OTEL_RESOURCE_ATTRIBUTES');
+        if (resourceAttrList) {
+            this._config.resource.attributes_list = resourceAttrList;
+        }
+    }
+
+    getConfig() {
+        return this._config;
+    }
+}
