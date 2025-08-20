7af6ebc4b9db4f096fc6d77f61329bd312426892
github-action-benchmark <github@users.noreply.github.com>
2025-08-20 22:16:21 +0000

    add Benchmark (benchmarkjs) benchmark result for a8a641a1c7979217aa80fdcd693857e4c901d26d

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1755718220607,
+  "lastUpdate": 1755728181448,
   "repoUrl": "https://github.com/open-telemetry/opentelemetry-js",
   "entries": {
     "Benchmark": [
@@ -36196,6 +36196,58 @@ window.BENCHMARK_DATA = {
             "range": "±0.56%",
             "unit": "ops/sec",
             "extra": "91 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "arya.mohanan@ibm.com",
+            "name": "Arya",
+            "username": "aryamohanan"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a8a641a1c7979217aa80fdcd693857e4c901d26d",
+          "message": "chore: remove cross-var dependency (was used in precompile scripts) (#5864)\n\nCo-authored-by: Trent Mick <trentm@gmail.com>\nCo-authored-by: Marylia Gutierrez <marylia.gutierrez@grafana.com>",
+          "timestamp": "2025-08-20T21:49:10Z",
+          "tree_id": "b60995b802abeec8f87efcfca91128a811da5760",
+          "url": "https://github.com/open-telemetry/opentelemetry-js/commit/a8a641a1c7979217aa80fdcd693857e4c901d26d"
+        },
+        "date": 1755728180425,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "transform 1 span",
+            "value": 670288,
+            "range": "±1.26%",
+            "unit": "ops/sec",
+            "extra": "93 samples"
+          },
+          {
+            "name": "transform 100 spans",
+            "value": 8470,
+            "range": "±0.69%",
+            "unit": "ops/sec",
+            "extra": "95 samples"
+          },
+          {
+            "name": "create spans (10 attributes)",
+            "value": 295117,
+            "range": "±0.49%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
+          },
+          {
+            "name": "BatchSpanProcessor process span",
+            "value": 279651,
+            "range": "±0.66%",
+            "unit": "ops/sec",
+            "extra": "94 samples"
           }
         ]
       }
