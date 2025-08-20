d627fff7b0607baf6e958f08477bfa45215a8978
Raphaël Thériault <113933910+raphael-theriault-swi@users.noreply.github.com>
2025-08-20 15:20:07 -0400

    feat(exporter-otlp-*)!: support custom HTTP agents (#5719)
    Co-authored-by: Marc Pichler <marc.pichler@dynatrace.com>

--- experimental/packages/otlp-exporter-base/test/node/otlp-http-export-delegate.test.ts
+++ experimental/packages/otlp-exporter-base/test/node/otlp-http-export-delegate.test.ts
@@ -55,7 +55,7 @@ describe('createOtlpHttpExportDelegate', function () {
     const delegate = createOtlpHttpExportDelegate(
       {
         url: 'http://localhost:8083',
-        agentOptions: {},
+        agentFactory: () => new http.Agent(),
         compression: 'none',
         concurrencyLimit: 30,
         headers: () => ({}),
