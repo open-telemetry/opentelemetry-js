17a7658ffb954e0f78fb8ad13c71e640881797a1
David Luna <david.luna@elastic.co>
2025-09-16 17:00:41 +0200

    tests: add test for sendWithHttp method

--- experimental/packages/otlp-exporter-base/test/node/http-transport-utils.test.ts
+++ experimental/packages/otlp-exporter-base/test/node/http-transport-utils.test.ts
@@ -15,7 +15,7 @@
  */
 import * as http from 'http';
 import * as assert from 'assert';
-import { compressAndSend } from '../../src/transport/http-transport-utils';
+import { compressAndSend, sendWithHttp } from '../../src/transport/http-transport-utils';
 
 describe('compressAndSend', function () {
   it('compressAndSend on destroyed request should handle error', function (done) {
@@ -29,5 +29,45 @@ describe('compressAndSend', function () {
         done(e);
       }
     });
+  });
+});
+
+describe('sendWithHttp', function () {
+  const requestFn: typeof http.request = (opts: any, cb: any): http.ClientRequest => {
+    sendUserAgent = opts.headers['User-Agent'];
+    // const request = http.request(opts, cb);
+    // return request.destroy();
+    return http.request(opts, cb);
+  };
+  let sendUserAgent: string;
+
+  beforeEach(() => {
+    sendUserAgent = '';
+  });
+  
+  it('sends a request setting the provided user agent', function (done) {
+    let firstCallback = true;
+    sendWithHttp(
+      requestFn,
+      {
+        url: 'http://localhost:8080',
+        compression: 'gzip',
+        headers: () => ({}),
+        userAgent: 'Transport-User-Agent/1.2.3',
+      },
+      new http.Agent(),
+      Buffer.from([1, 2, 3]),
+      // TODO: the `onDone` callback is called twice because there are two error handlers
+      // - first is attached on the request created in `sendWithHttp`
+      // - second is attached on the pipe within `compressAndSend`
+      () => {
+        if (firstCallback) {
+          firstCallback = false;
+          assert.strictEqual(sendUserAgent, 'Transport-User-Agent/1.2.3');
+          done();
+        }
+      },
+      100,
+    )
   });
 });
