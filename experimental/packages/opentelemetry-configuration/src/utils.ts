da7762ddad048aef94f2a91b986b14303dcefe05
maryliag <marylia.gutierrez@grafana.com>
2025-10-08 11:47:56 -0400

    fix post merge

--- experimental/packages/opentelemetry-configuration/src/utils.ts
+++ experimental/packages/opentelemetry-configuration/src/utils.ts
@@ -157,33 +157,6 @@ export function getStringListFromConfigFile(
     .filter(s => s !== '');
 }
 
-/**
- * Retrieves a list of strings from a configuration file parameter
- * that is a list of objects.
- * e.g. Parsing the value of `composite` such as
- * composite:
- *  - tracecontext:
- *  - baggage:
- *  - b3:
- * returns ['tracecontext', 'baggage', 'b3']
- * @param value - The value from the config file.
- * @returns {string[] | undefined} - The list of strings or `undefined`.
- */
-export function getListFromObjectsFromConfigFile(
-  value: object[] | undefined
-): string[] | undefined {
-  if (value) {
-    const list: string[] = [];
-    for (let i = 0; i < value.length; i++) {
-      list.push(...Object.keys(value[i]));
-    }
-    if (list.length > 0) {
-      return list;
-    }
-  }
-  return undefined;
-}
-
 export function envVariableSubstitution(value: unknown): string | undefined {
   if (value == null) {
     return undefined;
