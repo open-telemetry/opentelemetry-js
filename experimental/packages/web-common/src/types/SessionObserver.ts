9145fd6675b3dea9903fe41ee1399dec88a15bc3
Martin Kuba <martin@martinkuba.com>
2025-10-09 00:22:10 -0700

    feat(web): Add session handling implementation (#5173)
    Co-authored-by: Marc Pichler <marc.pichler@dynatrace.com>

--- experimental/packages/web-common/src/types/SessionObserver.ts
+++ experimental/packages/web-common/src/types/SessionObserver.ts
@@ -14,16 +14,9 @@
  * limitations under the License.
  */
 
-export type { SessionProvider } from './types/SessionProvider';
-export {
-  createSessionSpanProcessor,
-  createSessionLogRecordProcessor,
-  createSessionManager,
-  createDefaultSessionIdGenerator,
-  createLocalStorageSessionStore,
-} from './utils';
-export type { Session } from './types/Session';
-export type { SessionIdGenerator } from './types/SessionIdGenerator';
-export type { SessionPublisher } from './types/SessionPublisher';
-export type { SessionObserver } from './types/SessionObserver';
-export type { SessionStore } from './types/SessionStore';
+import { Session } from './Session';
+
+export interface SessionObserver {
+  onSessionStarted(newSession: Session, previousSession?: Session): void;
+  onSessionEnded(session: Session): void;
+}
