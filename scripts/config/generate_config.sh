2368852cf19ff64ab430d64e72d7ec0b3c514a23
JamieDanielson <jamiedanielson@honeycomb.io>
2025-10-09 16:18:46 -0400

    add script for generating types from json schema

---
+++ scripts/config/generate_config.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR="${SCRIPT_DIR}/../../"
+
+# Get latest version by running `git tag -l --sort=version:refname | tail -1`
+# ... in git@github.com:open-telemetry/opentelemetry-configuration.git
+CONFIG_VERSION=v0.4.0
+
+# When running on windows and you are getting references to ";C" (like Telemetry;C)
+# then this is an issue with the bash shell, so first run the following in your shell:
+# export MSYS_NO_PATHCONV=1
+
+cd ${SCRIPT_DIR}
+
+rm -rf opentelemetry-configuration || true
+mkdir opentelemetry-configuration
+cd opentelemetry-configuration
+
+git init
+git remote add origin https://github.com/open-telemetry/opentelemetry-configuration.git
+git fetch origin "${CONFIG_VERSION}" --depth=1
+git reset --hard FETCH_HEAD
+cd ${SCRIPT_DIR}
+
+# Create output directory if it doesn't exist
+mkdir -p ${ROOT_DIR}/experimental/packages/opentelemetry-configuration/src/generated
+
+# Generate TypeScript types using quicktype
+npx quicktype \
+  -s schema \
+  ${SCRIPT_DIR}/opentelemetry-configuration/schema/opentelemetry_configuration.json \
+  -o ${ROOT_DIR}/experimental/packages/opentelemetry-configuration/src/generated/opentelemetry_configuration.ts
+
+# Ensure config compiles
+cd "${ROOT_DIR}/experimental/packages/opentelemetry-configuration"
+npm run compile
