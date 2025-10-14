2b8a2d23aa6fda89ad3b15a94c150261f5f8cb45
maryliag <marylia.gutierrez@grafana.com>
2025-10-14 23:00:42 +0200

    fixes with messed merge

--- experimental/packages/opentelemetry-configuration/src/FileConfigProvider.ts
+++ experimental/packages/opentelemetry-configuration/src/FileConfigProvider.ts
@@ -280,9 +280,6 @@ function parseConfigExporter(
     case ProviderType.TRACER:
       endpoint = 'http://localhost:4318/v1/traces';
       break;
-    case ProviderType.METER:
-      endpoint = 'http://localhost:4318/v1/metrics';
-      break;
     case ProviderType.LOGGER:
       endpoint = 'http://localhost:4318/v1/logs';
       break;
