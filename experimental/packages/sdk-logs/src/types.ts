13e96a7fc7335606a0909515f37c9539f53a7e2a
Jackson Weber <jacksonweber@microsoft.com>
2025-11-26 13:53:05 -0800

    Add further experimental flags.

--- experimental/packages/sdk-logs/src/types.ts
+++ experimental/packages/sdk-logs/src/types.ts
@@ -21,12 +21,16 @@ import { LogRecordProcessor } from './LogRecordProcessor';
 
 /**
  * A LoggerConfig defines various configurable aspects of a Logger's behavior.
+ * 
+ * @experimental This feature is in development as per the OpenTelemetry specification.
  */
 export interface LoggerConfig {
   /**
    * A boolean indication of whether the logger is enabled.
    * If a Logger is disabled, it behaves equivalently to a No-op Logger.
    * Defaults to false (loggers are enabled by default).
+   * 
+   * @experimental This feature is in development as per the OpenTelemetry specification.
    */
   disabled?: boolean;
 
