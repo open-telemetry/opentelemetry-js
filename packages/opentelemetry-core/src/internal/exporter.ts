885fb67adf8724deec8181d9afb813ad8eb744c9
Colin Ihrig <cjihrig@gmail.com>
2026-02-04 09:59:14 -0500

    refactor(core): remove unnecessary closure in `_export()` (#6360)

--- packages/opentelemetry-core/src/internal/exporter.ts
+++ packages/opentelemetry-core/src/internal/exporter.ts
@@ -33,9 +33,7 @@ export function _export<T>(
   return new Promise(resolve => {
     // prevent downstream exporter calls from generating spans
     context.with(suppressTracing(context.active()), () => {
-      exporter.export(arg, (result: ExportResult) => {
-        resolve(result);
-      });
+      exporter.export(arg, resolve);
     });
   });
 }
