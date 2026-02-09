983d9df3936fa6c53f4a69177b48ceebb35f755e
MUSKANNISHAD <mkumari40432@gmail.com>
2026-02-09 16:18:39 +0530

    chore(context-async-hooks): remove deprecated AsyncHooksContextManager export

--- packages/opentelemetry-context-async-hooks/src/index.ts
+++ packages/opentelemetry-context-async-hooks/src/index.ts
@@ -14,5 +14,4 @@
  * limitations under the License.
  */
 
-export { AsyncHooksContextManager } from './AsyncHooksContextManager';
 export { AsyncLocalStorageContextManager } from './AsyncLocalStorageContextManager';
