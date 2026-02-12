ed8cc5233507be3c97efebfc7e86fc06141182e9
trentm <trentm@users.noreply.github.com>
2026-02-12 18:45:45 +0000

    Deploying to gh-pages from @ open-telemetry/opentelemetry-js@ad92be4c2c1094745a85b0b7eeff1444a11b1b4a 🚀

--- media/config.ts
+++ media/config.ts
@@ -14,47 +14,110 @@
  * limitations under the License.
  */
 
-import { getNumberFromEnv } from '@opentelemetry/core';
-import { LogRecordLimits } from './types';
+import { diag } from '@opentelemetry/api';
+import { getNumberFromEnv, getStringFromEnv } from '@opentelemetry/core';
+import { Sampler } from './Sampler';
+import { AlwaysOffSampler } from './sampler/AlwaysOffSampler';
+import { AlwaysOnSampler } from './sampler/AlwaysOnSampler';
+import { ParentBasedSampler } from './sampler/ParentBasedSampler';
+import { TraceIdRatioBasedSampler } from './sampler/TraceIdRatioBasedSampler';
 
+const enum TracesSamplerValues {
+  AlwaysOff = 'always_off',
+  AlwaysOn = 'always_on',
+  ParentBasedAlwaysOff = 'parentbased_always_off',
+  ParentBasedAlwaysOn = 'parentbased_always_on',
+  ParentBasedTraceIdRatio = 'parentbased_traceidratio',
+  TraceIdRatio = 'traceidratio',
+}
+
+const DEFAULT_RATIO = 1;
+
+/**
+ * Load default configuration. For fields with primitive values, any user-provided
+ * value will override the corresponding default value. For fields with
+ * non-primitive values (like `spanLimits`), the user-provided value will be
+ * used to extend the default value.
+ */
+
+// object needs to be wrapped in this function and called when needed otherwise
+// envs are parsed before tests are ran - causes tests using these envs to fail
 export function loadDefaultConfig() {
   return {
+    sampler: buildSamplerFromEnv(),
     forceFlushTimeoutMillis: 30000,
-    logRecordLimits: {
+    generalLimits: {
       attributeValueLengthLimit:
-        getNumberFromEnv('OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT') ??
-        Infinity,
+        getNumberFromEnv('OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT') ?? Infinity,
       attributeCountLimit:
-        getNumberFromEnv('OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT') ?? 128,
+        getNumberFromEnv('OTEL_ATTRIBUTE_COUNT_LIMIT') ?? 128,
     },
-    includeTraceContext: true,
+    spanLimits: {
+      attributeValueLengthLimit:
+        getNumberFromEnv('OTEL_SPAN_ATTRIBUTE_VALUE_LENGTH_LIMIT') ?? Infinity,
+      attributeCountLimit:
+        getNumberFromEnv('OTEL_SPAN_ATTRIBUTE_COUNT_LIMIT') ?? 128,
+      linkCountLimit: getNumberFromEnv('OTEL_SPAN_LINK_COUNT_LIMIT') ?? 128,
+      eventCountLimit: getNumberFromEnv('OTEL_SPAN_EVENT_COUNT_LIMIT') ?? 128,
+      attributePerEventCountLimit:
+        getNumberFromEnv('OTEL_SPAN_ATTRIBUTE_PER_EVENT_COUNT_LIMIT') ?? 128,
+      attributePerLinkCountLimit:
+        getNumberFromEnv('OTEL_SPAN_ATTRIBUTE_PER_LINK_COUNT_LIMIT') ?? 128,
+    },
   };
 }
 
 /**
- * When general limits are provided and model specific limits are not,
- * configures the model specific limits by using the values from the general ones.
- * @param logRecordLimits User provided limits configuration
+ * Based on environment, builds a sampler, complies with specification.
  */
-export function reconfigureLimits(
-  logRecordLimits: LogRecordLimits
-): Required<LogRecordLimits> {
-  return {
-    /**
-     * Reassign log record attribute count limit to use first non null value defined by user or use default value
-     */
-    attributeCountLimit:
-      logRecordLimits.attributeCountLimit ??
-      getNumberFromEnv('OTEL_LOGRECORD_ATTRIBUTE_COUNT_LIMIT') ??
-      getNumberFromEnv('OTEL_ATTRIBUTE_COUNT_LIMIT') ??
-      128,
-    /**
-     * Reassign log record attribute value length limit to use first non null value defined by user or use default value
-     */
-    attributeValueLengthLimit:
-      logRecordLimits.attributeValueLengthLimit ??
-      getNumberFromEnv('OTEL_LOGRECORD_ATTRIBUTE_VALUE_LENGTH_LIMIT') ??
-      getNumberFromEnv('OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT') ??
-      Infinity,
-  };
+export function buildSamplerFromEnv(): Sampler {
+  const sampler =
+    getStringFromEnv('OTEL_TRACES_SAMPLER') ??
+    TracesSamplerValues.ParentBasedAlwaysOn;
+  switch (sampler) {
+    case TracesSamplerValues.AlwaysOn:
+      return new AlwaysOnSampler();
+    case TracesSamplerValues.AlwaysOff:
+      return new AlwaysOffSampler();
+    case TracesSamplerValues.ParentBasedAlwaysOn:
+      return new ParentBasedSampler({
+        root: new AlwaysOnSampler(),
+      });
+    case TracesSamplerValues.ParentBasedAlwaysOff:
+      return new ParentBasedSampler({
+        root: new AlwaysOffSampler(),
+      });
+    case TracesSamplerValues.TraceIdRatio:
+      return new TraceIdRatioBasedSampler(getSamplerProbabilityFromEnv());
+    case TracesSamplerValues.ParentBasedTraceIdRatio:
+      return new ParentBasedSampler({
+        root: new TraceIdRatioBasedSampler(getSamplerProbabilityFromEnv()),
+      });
+    default:
+      diag.error(
+        `OTEL_TRACES_SAMPLER value "${sampler}" invalid, defaulting to "${TracesSamplerValues.ParentBasedAlwaysOn}".`
+      );
+      return new ParentBasedSampler({
+        root: new AlwaysOnSampler(),
+      });
+  }
+}
+
+function getSamplerProbabilityFromEnv(): number | undefined {
+  const probability = getNumberFromEnv('OTEL_TRACES_SAMPLER_ARG');
+  if (probability == null) {
+    diag.error(
+      `OTEL_TRACES_SAMPLER_ARG is blank, defaulting to ${DEFAULT_RATIO}.`
+    );
+    return DEFAULT_RATIO;
+  }
+
+  if (probability < 0 || probability > 1) {
+    diag.error(
+      `OTEL_TRACES_SAMPLER_ARG=${probability} was given, but it is out of range ([0..1]), defaulting to ${DEFAULT_RATIO}.`
+    );
+    return DEFAULT_RATIO;
+  }
+
+  return probability;
 }
