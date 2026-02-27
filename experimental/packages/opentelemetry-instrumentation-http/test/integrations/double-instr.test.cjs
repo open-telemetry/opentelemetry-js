8bcbdafbbe6d8b5fad430dd6473e7265e2250390
overbalance <overbalance@users.noreply.github.com>
2026-02-27 18:17:40 -0500

    fix link to built file

--- experimental/packages/opentelemetry-instrumentation-http/test/integrations/double-instr.test.cjs
+++ experimental/packages/opentelemetry-instrumentation-http/test/integrations/double-instr.test.cjs
@@ -1,17 +1,6 @@
 /*
  * Copyright The OpenTelemetry Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 // Test that http/https are not *double*-instrumented if http is loaded by
@@ -33,7 +22,7 @@ const {
   SimpleSpanProcessor,
 } = require('@opentelemetry/sdk-trace-base');
 
-const { HttpInstrumentation } = require('../../build/src/index.js');
+const { HttpInstrumentation } = require('../../build/index.cjs');
 
 const memoryExporter = new InMemorySpanExporter();
 const provider = new NodeTracerProvider({
