9f6d563b51deb8d63c74eabac5055acf3e88bec5
maryliag <marylia.gutierrez@grafana.com>
2026-03-03 13:34:14 -0500

    export interfaces required in other packages

--- experimental/packages/configuration/src/index.ts
+++ experimental/packages/configuration/src/index.ts
@@ -6,6 +6,11 @@
 export type { ConfigFactory } from './IConfigFactory';
 export type { ConfigurationModel } from './models/configModel';
 export type { LogRecordExporter as LogRecordExporterModel } from './models/loggerProviderModel';
-export type { PushMetricExporter as MeterExporterModel } from './models/meterProviderModel';
+export type {
+  PushMetricExporter as MeterExporterModel,
+  InstrumentType,
+  Aggregation,
+  PeriodicMetricReader,
+} from './models/meterProviderModel';
 export type { SpanExporter as SpanExporterModel } from './models/tracerProviderModel';
 export { createConfigFactory } from './ConfigFactory';
