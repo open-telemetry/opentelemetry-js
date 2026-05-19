86d391e19e93b28cc893867587bfd7f55bf9ddc6
David Luna <david.luna@elastic.co>
2026-05-19 12:47:40 +0200

    chore: remove internal export

--- packages/opentelemetry-sdk-trace-base/src/index.ts
+++ packages/opentelemetry-sdk-trace-base/src/index.ts
@@ -4,7 +4,6 @@
  */
 
 export { BasicTracerProvider } from './BasicTracerProvider';
-export { TracerProvider } from './TracerProvider';
 export { BatchSpanProcessor, RandomIdGenerator } from './platform';
 export { ConsoleSpanExporter } from './export/ConsoleSpanExporter';
 export { InMemorySpanExporter } from './export/InMemorySpanExporter';
