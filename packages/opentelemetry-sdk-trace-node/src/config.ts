5328a8fea6f79faaa4ce5cd62157282d004905d3
David Luna <david.luna@elastic.co>
2026-05-26 09:43:58 +0200

    Update packages/opentelemetry-sdk-trace-node/src/config.ts
    Co-authored-by: Trent Mick <trentm@gmail.com>

--- packages/opentelemetry-sdk-trace-node/src/config.ts
+++ packages/opentelemetry-sdk-trace-node/src/config.ts
@@ -8,7 +8,6 @@ import type { TracerConfig } from '@opentelemetry/sdk-trace-base';
 /**
  * NodeTracerConfig provides an interface for configuring a Node Tracer.
  *
- * @deprecated this interface and package will be removed in next major (3.0) and replaced by
- * the `TracerProviderConfig` interface of the future `@opentelemetry/sdk-trace` package.
+ * @deprecated Use `TracerConfig` from the `@opentelemetry/sdk-trace-base` package. (In the next major (3.0) these will be replaced by a `TracerProviderOptions` type from a `@opentelemetry/sdk-trace` package.
  */
 export type NodeTracerConfig = TracerConfig;
