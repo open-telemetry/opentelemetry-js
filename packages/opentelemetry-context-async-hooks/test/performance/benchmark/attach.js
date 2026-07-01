b5392e8d19e00c8e582a66f8ba6d77820d83be82
Marc Pichler <marc.pichler@dynatrace.com>
2026-07-01 13:50:11 +0200

    test(async-context): add benchmark script

---
+++ packages/opentelemetry-context-async-hooks/test/performance/benchmark/attach.js
@@ -0,0 +1,54 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+const Benchmark = require('benchmark');
+const {
+  AsyncLocalStorageContextManager,
+} = require('../../../build/src');
+const { createContextKey, ROOT_CONTEXT } = require('@opentelemetry/api');
+
+const contextManager = new AsyncLocalStorageContextManager();
+contextManager.enable();
+
+const key = createContextKey('benchmark-key');
+const context1 = ROOT_CONTEXT.setValue(key, 'value1');
+const context2 = ROOT_CONTEXT.setValue(key, 'value2');
+const context3 = ROOT_CONTEXT.setValue(key, 'value3');
+
+const suite = new Benchmark.Suite();
+
+suite.on('cycle', event => {
+  console.log(String(event.target));
+});
+
+suite.add('attach + dispose (single)', function () {
+  const token = contextManager.attach(context1);
+  contextManager.detach(context1);
+});
+
+suite.add('attach + dispose (nested 3 levels)', function () {
+  const token1 = contextManager.attach(context1);
+  const token2 = contextManager.attach(context2);
+  const token3 = contextManager.attach(context3);
+  contextManager.detach(context3);
+  contextManager.detach(context2);
+  contextManager.detach(context1);
+});
+
+suite.add('with() (baseline, single)', function () {
+  contextManager.with(context1, () => {});
+});
+
+suite.add('with() (baseline, nested 3 levels)', function () {
+  contextManager.with(context1, () => {
+    contextManager.with(context2, () => {
+      contextManager.with(context3, () => {});
+    });
+  });
+});
+
+suite.run();
