ae63871ab5f2e5ba782754527e8c4283736130dd
Matthew Wear <matthew.wear@gmail.com>
2026-07-02 09:35:17 -0700

    feat(api-config): add ConfigProvider + ConfigProperties API

---
+++ experimental/packages/api-config/src/internal/global-utils.ts
@@ -0,0 +1,43 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import type { ConfigProvider } from '../types/ConfigProvider';
+
+export const GLOBAL_CONFIG_API_KEY = Symbol.for(
+  'io.opentelemetry.js.api.config'
+);
+
+type Get<T> = (version: number) => T;
+type OtelGlobal = Partial<{
+  [GLOBAL_CONFIG_API_KEY]: Get<ConfigProvider>;
+}>;
+
+export const _global = globalThis as OtelGlobal;
+
+/**
+ * Make a function which accepts a version integer and returns the instance of an API if the version
+ * is compatible, or a fallback version (usually NOOP) if it is not.
+ *
+ * @param requiredVersion Backwards compatibility version which is required to return the instance
+ * @param instance Instance which should be returned if the required version is compatible
+ * @param fallback Fallback instance, usually NOOP, which will be returned if the required version is not compatible
+ */
+export function makeGetter<T>(
+  requiredVersion: number,
+  instance: T,
+  fallback: T
+): Get<T> {
+  return (version: number): T =>
+    version === requiredVersion ? instance : fallback;
+}
+
+/**
+ * A number which should be incremented each time a backwards incompatible
+ * change is made to the API. This number is used when an API package
+ * attempts to access the global API to ensure it is getting a compatible
+ * version. If the global API is not compatible with the API package
+ * attempting to get it, a NOOP API implementation will be returned.
+ */
+export const API_BACKWARDS_COMPATIBILITY_VERSION = 1;
