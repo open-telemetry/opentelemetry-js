569b27f710b9e7485c8f980a57a271bf6194a6e5
Trent Mick <trentm@gmail.com>
2026-08-12 08:26:49 -0700

    use semconv pkg const for this

--- packages/opentelemetry-resources/test/detectors/node/ServiceInstanceIdDetector.test.ts
+++ packages/opentelemetry-resources/test/detectors/node/ServiceInstanceIdDetector.test.ts
@@ -6,13 +6,14 @@
 import * as assert from 'assert';
 import { detectResources, serviceInstanceIdDetector } from '../../../src';
 import { describeNode } from '../../util';
+import { ATTR_SERVICE_INSTANCE_ID } from '@opentelemetry/semantic-conventions';
 
 describeNode('serviceInstanceIdDetector', () => {
   it('should return a UUID for service.instance.id', async () => {
     const attrs = detectResources({
       detectors: [serviceInstanceIdDetector],
     }).attributes;
-    assert.deepEqual(Object.keys(attrs), ['service.instance.id']);
+    assert.deepEqual(Object.keys(attrs), [ATTR_SERVICE_INSTANCE_ID]);
     assert.match(
       (attrs as any)['service.instance.id'],
       /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
